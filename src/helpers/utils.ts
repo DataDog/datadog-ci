e947883252abedfaab4934f494b4f88bb85a9b71
Yoann Moinet <yoann.moinet@datadoghq.com>
2019-07-01 15:42:36 +0200

    Add some output

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import * as glob from 'glob';
+import * as path from 'path';
 import { promisify } from 'util';
 
 import { Suite } from './interfaces';
@@ -22,7 +23,12 @@ export const stopIntervals = (interval: NodeJS.Timeout, timeout: NodeJS.Timeout)
 };
 
 export const getSuites = (GLOB: string): Suite[] => {
+    console.log(`Finding files in ${path.join(process.cwd(), GLOB)}`);
     return promisify((glob as any).glob)(GLOB)
+        .then((files: string[]) => {
+            console.log(`Got test files:\n${JSON.stringify(files)}`);
+            return files;
+        })
         .then((files: string[]) => files.map((test) => fs.readFile(test, 'utf8')))
         .then((promises: Array<Promise<string>>) => Promise.all(promises))
         .then((contents: string[]) => contents.map((content) => JSON.parse(content)));
