5f1a61bcb7dbfedabed2b8f11afd6074acc7cd68
Yoann Moinet <yoann.moinet@datadoghq.com>
2019-07-04 16:48:44 +0200

    executable

--- bin/make-it-executable.js
+++ bin/make-it-executable.js
@@ -1,11 +1,8 @@
 const path = require('path');
 const fs = require('fs').promises;
-const { exec } = require('child_process');
-const { promisify } = require('util');
 
 const ROOT = path.join(__dirname, '../');
 const scriptPaths = require(path.join(ROOT, './package.json')).bin;
-const execute = promisify(exec);
 
 const INSERT = '#!/usr/bin/env node';
 
