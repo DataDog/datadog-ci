df8d5ef0b18a76bdaa85a02bfb749b0d834747dc
Yoann Moinet <yoann.moinet@datadoghq.com>
2020-01-13 16:06:24 +0100

    Better log for API errors

--- src/helpers/dd-api.ts
+++ src/helpers/dd-api.ts
@@ -1,6 +1,6 @@
 import { Options } from 'request';
 import { defaults as requestDefaults, RequestPromise } from 'request-promise-native';
-import { APIConstructor, Payload, PollResult, Test, Trigger } from './interfaces';
+import { Payload, PollResult, Test, Trigger } from './interfaces';
 
 const triggerTests = (request: (args: Options) => RequestPromise<Trigger>) => (testIds: string[], config?: Payload) =>
   request({
@@ -10,11 +10,17 @@ const triggerTests = (request: (args: Options) => RequestPromise<Trigger>) => (t
     },
     method: 'POST',
     uri: '/synthetics/tests/trigger',
+  }).catch(e => {
+    throw new Error(`Could not trigger [${testIds}].
+${e.statusCode}: ${e.name}`);
   });
 
 const getTest = (request: (args: Options) => RequestPromise<Test>) => (testId: string) =>
   request({
     uri: `/synthetics/tests/${testId}`,
+  }).catch(e => {
+    throw new Error(`Could not get test ${testId}.
+${e.statusCode}: ${e.name}`);
   });
 
 const pollResults = (request: (args: Options) => RequestPromise<{ results: PollResult[] }>) => (resultIds: string[]) =>
@@ -23,9 +29,12 @@ const pollResults = (request: (args: Options) => RequestPromise<{ results: PollR
       result_ids: JSON.stringify(resultIds),
     },
     uri: '/synthetics/tests/poll_results',
+  }).catch(e => {
+    throw new Error(`Could not poll results [${resultIds}].
+${e.statusCode}: ${e.name}`);
   });
 
-export const apiConstructor: APIConstructor = ({ appKey, apiKey, baseUrl }) => {
+export const apiConstructor: any = ({ appKey, apiKey, baseUrl }: any) => {
   const request = (args: Options) =>
     requestDefaults({
         baseUrl,
