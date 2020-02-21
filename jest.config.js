c736714c86adfc6bdc39811c2134504cb006342e
Mathieu Rousse <mathieu.rousse@datadoghq.com>
2020-02-21 17:52:10 +0100

    [jest] review fixes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
 module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
