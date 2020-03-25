40317723c3a79091e726853f9c40bc060e07684a
Mathieu Rousse <mathieu.rousse@datadoghq.com>
2020-03-25 15:48:06 +0100

    [synthetics] support full url instead of public id

--- src/commands/synthetics/__tests__/utils.test.ts
+++ src/commands/synthetics/__tests__/utils.test.ts
@@ -2,9 +2,12 @@ jest.mock('glob');
 jest.useFakeTimers();
 
 import * as fs from 'fs';
-import glob from 'glob';
 
-import { getSuites } from '../utils';
+import glob from 'glob';
+import request from 'request-promise-native';
+
+import { apiConstructor } from '../api';
+import { getSuites, runTest } from '../utils';
 
 describe('utils', () => {
   describe('getSuites', () => {
@@ -21,6 +24,44 @@ describe('utils', () => {
     test('should get suites', async () => {
       const suites = await getSuites(GLOB, process.stdout.write.bind(process.stdout));
       expect(JSON.stringify(suites)).toBe(`[${FILES_CONTENT.file1},${FILES_CONTENT.file2}]`);
+    });
+  });
+
+  describe('runTest', () => {
+    const api = apiConstructor({ apiKey: '123', appKey: '123', baseUrl: 'base' });
+    const processWrite = process.stdout.write.bind(process.stdout);
+    const fakeTest = {
+      name: 'Fake Test',
+      public_id: '123-456-789',
+    };
+    const fakeTrigger = {
+      results: [],
+      triggered_check_ids: [fakeTest.public_id],
+    };
+    jest.spyOn(request, 'defaults').mockImplementation((() => (e: request.RequestPromise) => {
+      if (e.uri as any === '/synthetics/tests/trigger/ci') {
+        return fakeTrigger;
+      }
+
+      if (e.uri as any === `/synthetics/tests/${fakeTest.public_id}`) {
+        return fakeTest;
+      }
+    }) as any);
+
+    test('should run test', async () => {
+      const output = await runTest(api, { id: fakeTest.public_id, config: { } }, processWrite);
+      expect(output).toEqual([fakeTest, fakeTrigger.results]);
+    });
+
+    test('should run test with publicId from url', async () => {
+      const output = await runTest(
+        api, {
+          config: { },
+          id: `http://localhost/synthetics/tests/details/${fakeTest.public_id}`,
+        },
+        processWrite
+      );
+      expect(output).toEqual([fakeTest, fakeTrigger.results]);
     });
   });
 });
