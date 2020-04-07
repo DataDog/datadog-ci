6c6d097efab4c81a39da1c137c7860177106dff6
Mathieu Rousse <mathieu.rousse@datadoghq.com>
2020-04-07 19:41:56 +0200

    Support node 8 and 10

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,7 +1,10 @@
-export function pick<T extends object, K extends keyof T> (base: T, keys: K[]): Pick<T, K> {
-  const entries = keys
-    .filter(key => !!base[key])
-    .map(key => ([key, base[key]]));
+export function pick<T extends object, K extends keyof T> (base: T, keys: K[]) {
+  const definedKeys = keys.filter(key => !!base[key]);
+  const pickedObject: Partial<T> = { };
 
-  return Object.fromEntries(entries);
+  for (const key of definedKeys) {
+    pickedObject[key] = base[key];
+  }
+
+  return pickedObject;
 }
