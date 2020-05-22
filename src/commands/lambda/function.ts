04b73bc216b30bd9a17eb523886d5c98712ba3cd
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-05-22 13:37:58 -0400

    [lambda] Fetch lambda configs from options

--- src/commands/lambda/function.ts
+++ src/commands/lambda/function.ts
@@ -1,5 +1,112 @@
 import {Lambda} from 'aws-sdk'
+import {DEFAULT_LAYER_AWS_ACCOUNT, HANDLER_LOCATION, Runtime, RUNTIME_LAYER_LOOKUP} from './constants'
 
-export const getLambdaConfig = async (lambda: Lambda, functionArn: string) => {
+export interface FunctionConfiguration {
+  functionARN: string
+  lambdaConfig: Lambda.FunctionConfiguration
+  layerARN: string
+}
+
+export interface InstrumentationSettings {
+  forwarderARN?: string
+  layerAWSAccount?: string
+  layerVersion?: number
+  mergeXrayTraces: boolean
+  region: string
+  tracingEnabled: boolean
+}
+
+export const getLambdaConfigs = async (lambda: Lambda, functionARNs: string[], settings: InstrumentationSettings) => {
+  const resultPromises = functionARNs.map((fn) => getLambdaConfig(lambda, fn))
+  const results = await Promise.all([...resultPromises])
+  const layerARNLookup: {[key: string]: string} = {}
+  const functionsToUpdate: FunctionConfiguration[] = []
+
+  for (const {config, functionARN} of results) {
+    if (config === undefined) {
+      throw Error(`Failed to get config of ${functionARN}`)
+    }
+    const runtime = config.Runtime as Runtime
+    if (runtime === undefined || RUNTIME_LAYER_LOOKUP[runtime] === undefined) {
+      throw Error(`Can't instrument ${functionARN}, runtime ${runtime} not supported`)
+    }
+    let layerARN: string | undefined = layerARNLookup[runtime]
+    if (layerARN === undefined) {
+      layerARN = await getLayerArn(lambda, runtime, settings)
+      if (layerARN === undefined) {
+        throw Error(`Couldn't find layer for runtime ${runtime}, used by function ${functionARN}`)
+      }
+      layerARNLookup[runtime] = layerARN
+    }
+    if (configNeedsUpdating(config, settings, layerARN, runtime)) {
+      functionsToUpdate.push({functionARN, layerARN, lambdaConfig: config})
+    }
+  }
+
+  return functionsToUpdate
+}
+
+export const updateLambdaConfigs = async (
+  lambda: Lambda,
+  configurations: FunctionConfiguration[],
+  settings: InstrumentationSettings
+) => {
+  const results = configurations.map((c) => updateLambdaConfig(lambda, c, settings))
+  await Promise.all([...results])
+}
+
+const getLambdaConfig = async (lambda: Lambda, functionARN: string) => {
+  const params = {
+    FunctionName: functionARN,
+  }
+  const result = await lambda.getFunction(params).promise()
+
+  return {config: result.Configuration, functionARN}
+}
+
+const updateLambdaConfig = async (
+  lambda: Lambda,
+  configuration: FunctionConfiguration,
+  settings: InstrumentationSettings
+) => {
   throw Error('Unimplemented')
+}
+
+const getLayerArn = async (lambda: Lambda, runtime: Runtime, settings: InstrumentationSettings) => {
+  const {layerVersion, region} = settings
+  const layerName = RUNTIME_LAYER_LOOKUP[runtime]
+  const account = settings.layerAWSAccount ?? DEFAULT_LAYER_AWS_ACCOUNT
+
+  return `arn:aws:lambda:${region}:${account}:layer:${layerName}:${layerVersion}`
+}
+
+const configNeedsUpdating = async (
+  config: Lambda.FunctionConfiguration,
+  settings: InstrumentationSettings,
+  layerARN: string,
+  runtime: Runtime
+) => {
+  const env = config.Environment?.Variables
+  if (env === undefined) {
+    return true
+  }
+  if (env.DD_LAMBDA_HANDLER === undefined) {
+    return true
+  }
+  const expectedHandler = HANDLER_LOCATION[runtime]
+  if (config.Handler !== expectedHandler) {
+    return true
+  }
+  const layerARNs = (config.Layers ?? []).map((layer) => layer.Arn)
+  if (!layerARNs.includes(layerARN)) {
+    return true
+  }
+  if (env.DATADOG_TRACE_ENABLED !== settings.tracingEnabled.toString()) {
+    return true
+  }
+  if (env.DATADOG_MERGE_XRAY_TRACES !== settings.mergeXrayTraces.toString()) {
+    return true
+  }
+
+  return false
 }
--- src/commands/lambda/instrument.ts
+++ src/commands/lambda/instrument.ts
@@ -1,18 +1,44 @@
 import {Lambda} from 'aws-sdk'
 import {Command} from 'clipanion'
-import {getLambdaConfig} from './function'
+import {getLambdaConfigs, InstrumentationSettings} from './function'
 
 export class InstrumentCommand extends Command {
+  private dryRun = false
+  private forwarderARN?: string
   private functions: string[] = []
+  private layerAWSAccount?: string
+  private layerVersion?: string
   private region?: string
 
   public async execute() {
+    if (this.layerVersion === undefined) {
+      this.context.stdout.write('No layer version specified. Use -v,--layerVersion\n')
+
+      return 1
+    }
+    const layerVersion = parseInt(this.layerVersion, 10)
+    if (Number.isNaN(layerVersion)) {
+      this.context.stdout.write(`Invalid layer version ${layerVersion}.\n`)
+
+      return 1
+    }
+
+    const settings: InstrumentationSettings = {
+      forwarderARN: this.forwarderARN,
+      layerAWSAccount: this.layerAWSAccount,
+      layerVersion,
+      mergeXrayTraces: false,
+      region: this.region ?? 'us-east-1',
+      tracingEnabled: false,
+    }
+
     if (this.functions.length === 0) {
       this.context.stdout.write('No functions specified for implementation.\n')
     }
     const lambda = new Lambda({region: this.region})
-    for (const func of this.functions) {
-      getLambdaConfig(lambda, func)
+    const configs = await getLambdaConfigs(lambda, this.functions, settings)
+    if (this.dryRun) {
+      console.log(JSON.stringify(configs, undefined, 3))
     }
 
     return 0
@@ -22,4 +48,7 @@ export class InstrumentCommand extends Command {
 InstrumentCommand.addPath('lambda', 'instrument')
 InstrumentCommand.addOption('functions', Command.Array('-f,--function'))
 InstrumentCommand.addOption('region', Command.String('-r,--region'))
-InstrumentCommand.addOption('forwarder', Command.String('--forwarder'))
+InstrumentCommand.addOption('layerVersion', Command.String('-v,--layerVersion'))
+InstrumentCommand.addOption('layerAWSAccount', Command.String('-a,--layerAccount'))
+InstrumentCommand.addOption('forwarderARN', Command.String('--forwarder'))
+InstrumentCommand.addOption('dryRun', Command.Boolean('-d,--dry'))
