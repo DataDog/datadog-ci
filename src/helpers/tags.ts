3488d92d61a09c61f3a0cffc86a468c74b21a5d8
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-03-10 16:29:38 +0100

    [ci-app] add tag and improve stdio

--- src/helpers/tags.ts
+++ src/helpers/tags.ts
@@ -8,7 +8,13 @@ export const CI_ENV_TRACE_ID = 'X_DATADOG_TRACE_ID'
 // Build
 export const CI_PIPELINE_URL = 'ci.pipeline.url'
 export const CI_PROVIDER_NAME = 'ci.provider.name'
+export const CI_LEVEL = '_dd.ci.level'
+// @deprecated TODO: remove this once backend is updated
+export const CI_BUILD_LEVEL = '_dd.ci.build_level'
 
 // Git
 export const GIT_BRANCH = 'git.branch'
 export const GIT_SHA = 'git.commit.sha'
+
+// General
+export const SPAN_TYPE = 'span.type'
