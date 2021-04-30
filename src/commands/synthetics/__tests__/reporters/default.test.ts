109b99129b1d2890e53df6f3f60fcf1726c4fc7c
yoannmoinet <yoann.moinet@datadoghq.com>
2021-04-30 11:28:28 +0200

    Add test for default reporter

---
+++ src/commands/synthetics/__tests__/reporters/default.test.ts
@@ -0,0 +1,31 @@
+import {RunTestCommand} from '../../run-test'
+import {DefaultReporter} from '../../reporters/default'
+
+describe('Default reporter', () => {
+  const writeMock = jest.fn()
+  const commandMock: unknown = {
+    context: {
+      stdout: {
+        write: writeMock,
+      },
+    },
+  }
+  const reporter: any = new DefaultReporter(commandMock as RunTestCommand)
+  it('should log for each hook', () => {
+    const calls: [string, any[]][] = [
+      ['error', ['error']],
+      ['initError', [['error']]],
+      ['log', ['log']],
+      ['runEnd', [{passed: 0, failed: 0, skipped: 0}]],
+      ['start', [{startTime: 0}]],
+      ['testEnd', [{options: {}}, [], '', []]],
+      ['testTrigger', [{}, '', '', {}]],
+      ['testWait', [{}]],
+    ]
+    for (const [fnName, args] of calls) {
+      reporter[fnName](...args)
+      expect(writeMock).toHaveBeenCalledTimes(1)
+      writeMock.mockClear()
+    }
+  })
+})
