15f329e825aa3bf4c7fee0ba27fda4570f82c8fa
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2021-06-08 13:16:36 +0200

    [ci-app] add rich commit metadata

--- src/helpers/git.ts
+++ src/helpers/git.ts
@@ -1,6 +1,17 @@
 import simpleGit from 'simple-git'
 
-import {GIT_BRANCH, GIT_REPOSITORY_URL, GIT_SHA} from './tags'
+import {
+  GIT_BRANCH,
+  GIT_COMMIT_AUTHOR_DATE,
+  GIT_COMMIT_AUTHOR_EMAIL,
+  GIT_COMMIT_AUTHOR_NAME,
+  GIT_COMMIT_COMMITTER_DATE,
+  GIT_COMMIT_COMMITTER_EMAIL,
+  GIT_COMMIT_COMMITTER_NAME,
+  GIT_COMMIT_MESSAGE,
+  GIT_REPOSITORY_URL,
+  GIT_SHA,
+} from './tags'
 
 export const getGitMetadata = async () => {
   try {
@@ -11,16 +22,34 @@ export const getGitMetadata = async () => {
       maxConcurrentProcesses: 3,
     })
 
-    const [commitSHA, branch, repositoryUrl] = await Promise.all([
+    const [commitSHA, branch, repositoryUrl, message, authorAndCommitter] = await Promise.all([
       git.revparse('HEAD'),
       git.branch(),
       git.listRemote(['--get-url']),
+      git.show('--format=%s'),
+      git.show('--format=%an,%ae,%ad,%cn,%ce,%cd'),
     ])
+
+    const [
+      authorName,
+      authorEmail,
+      authorDate,
+      committerName,
+      committerEmail,
+      committerDate,
+    ] = authorAndCommitter.split(',')
 
     return {
       [GIT_REPOSITORY_URL]: repositoryUrl.trim(),
       [GIT_BRANCH]: branch.current,
       [GIT_SHA]: commitSHA,
+      [GIT_COMMIT_MESSAGE]: message.trim(),
+      [GIT_COMMIT_COMMITTER_DATE]: committerDate.trim(),
+      [GIT_COMMIT_COMMITTER_EMAIL]: committerEmail.trim(),
+      [GIT_COMMIT_COMMITTER_NAME]: committerName.trim(),
+      [GIT_COMMIT_AUTHOR_DATE]: authorDate.trim(),
+      [GIT_COMMIT_AUTHOR_EMAIL]: authorEmail.trim(),
+      [GIT_COMMIT_AUTHOR_NAME]: authorName.trim(),
     }
   } catch (e) {
     return {}
