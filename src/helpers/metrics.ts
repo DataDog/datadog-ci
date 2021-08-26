e62311a085ee68dab2d49edc7c2c63836984c314
Loic Raucy <loic.raucy@datadoghq.com>
2021-08-26 15:18:33 +0200

    Support proxy for reporting metrics.

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -16,6 +16,9 @@ export const getMetricsLogger = (opts: MetricsLoggerOptions): MetricsLogger => {
   const apiUrl = getBaseAPIUrl(opts.datadogSite)
 
   const metricsOpts = {
+    // ProxyAgent will retrieve proxy agent from env vars when relevant
+    // agent is not in the type definitions file but has been introduced in datadog-metrics 0.8.x
+    agent: new ProxyAgent(),
     apiHost: apiUrl,
     defaultTags: opts.defaultTags,
     flushIntervalSeconds: 15,
