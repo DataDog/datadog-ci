5ebe1fe677bf649271927c6a9195cada430039ad
Adrian Lopez <adrian.lopezcalvo@datadoghq.com>
2021-09-06 15:52:00 +0200

    Update DNS

--- src/commands/junit/utils.ts
+++ src/commands/junit/utils.ts
@@ -1,9 +1,9 @@
 export const getBaseIntakeUrl = () => {
   if (process.env.DATADOG_SITE || process.env.DD_SITE) {
-    return `https://cireport-http-intake.${process.env.DATADOG_SITE || process.env.DD_SITE}`
+    return `https://cireport-intake.${process.env.DATADOG_SITE || process.env.DD_SITE}`
   }
 
-  return 'https://cireport-http-intake.datadoghq.com'
+  return 'https://cireport-intake.datadoghq.com'
 }
 
 /**
