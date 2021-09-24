010efb547e570d0b6f88bfaf2f0c32ab3fc6840f
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2021-09-24 13:23:24 -0400

    add config command and check if functions have been specified

--- src/commands/lambda/uninstrument.ts
+++ src/commands/lambda/uninstrument.ts
@@ -1,22 +1,31 @@
 import { Command } from 'clipanion'
-import { LambdaConfigOptions } from './interfaces'
+import { parseConfigFile } from '../../helpers/utils'
 
 export class UninstrumentCommand extends Command {
-    private config: LambdaConfigOptions = {
-        functions: [],
-        region: process.env.AWS_DEFAULT_REGION,
-    }
+  private config: any = {
+    functions: [],
+    region: process.env.AWS_DEFAULT_REGION
+  }
+  private dryRun = false
+  private functions: string[] = []
+  private region?: string
+  private configPath?: string
 
-    private dryRun = false
-    private functions: string[] = []
-    private region?: string
+  public async execute() {
+    const lambdaConfig = {lambda: this.config}
+    this.config = (await parseConfigFile(lambdaConfig, this.configPath)).lambda
+    
+    const hasSpecifiedFuntions = this.functions.length !== 0 || this.config.functions.length !== 0
+    if (!hasSpecifiedFuntions) {
+      this.context.stdout.write('No functions specified for un-instrumentation.\n')
 
-    public async execute() {
-        console.log('wow much uninstrument, so serverless!!!!1eleven')
-    }
+      return 1
+    }    
+  }
 }
 
 UninstrumentCommand.addPath('lambda', 'uninstrument')
 UninstrumentCommand.addOption('functions', Command.Array('-f,--function'))
 UninstrumentCommand.addOption('region', Command.String('-r,--region'))
+UninstrumentCommand.addOption('configPath', Command.Array('--config'))
 UninstrumentCommand.addOption('dryRun', Command.Boolean('-d,--dry'))
