09d66f93da885caeb2bd2d7cf56ff501bc3b4808
Ximo Guanter <ximo.guanter@datadoghq.com>
2021-10-05 07:42:49 +0200

    PR feedback

--- src/helpers/__tests__/retry.test.ts
+++ src/helpers/__tests__/retry.test.ts
@@ -34,6 +34,8 @@ describe('retry', () => {
 
   test('should retry retriable failed requests', async () => {
     await retryRequest(createResultWithErrors([buildHttpError(500), undefined]), {
+      maxTimeout: 50,
+      minTimeout: 10,
       onRetry: retryCallback,
       retries: 5,
     })
@@ -42,6 +44,8 @@ describe('retry', () => {
 
   test('should retry non-http errors', async () => {
     await retryRequest(createResultWithErrors([{message: 'Connection timeout'}, undefined]), {
+      maxTimeout: 50,
+      minTimeout: 10,
       onRetry: retryCallback,
       retries: 5,
     })
@@ -52,6 +56,8 @@ describe('retry', () => {
     let threwError = false
     try {
       await retryRequest(createResultWithErrors([buildHttpError(413)]), {
+        maxTimeout: 50,
+        minTimeout: 10,
         onRetry: retryCallback,
         retries: 5,
       })
@@ -65,21 +71,28 @@ describe('retry', () => {
   test('should retry only a given amount of times', async () => {
     let threwError = false
     try {
-      await retryRequest(createResultWithErrors([buildHttpError(413), buildHttpError(413)]), {
-        onRetry: retryCallback,
-        retries: 1,
-      })
+      await retryRequest(
+        createResultWithErrors([buildHttpError(500), buildHttpError(500), buildHttpError(500), buildHttpError(500)]),
+        {
+          maxTimeout: 20,
+          minTimeout: 10,
+          onRetry: retryCallback,
+          retries: 3,
+        }
+      )
     } catch (error) {
       threwError = true
     }
     expect(threwError).toBeTruthy()
-    expect(retryCallback).toBeCalledTimes(0)
+    expect(retryCallback).toBeCalledTimes(3)
   })
 
   test('should not retry if the call was successful', async () => {
     await retryRequest(createResultWithErrors([undefined]), {
+      maxTimeout: 50,
+      minTimeout: 10,
       onRetry: retryCallback,
-      retries: 1,
+      retries: 5,
     })
     expect(retryCallback).toBeCalledTimes(0)
   })
--- src/helpers/retry.ts
+++ src/helpers/retry.ts
@@ -8,18 +8,15 @@ export const retryRequest = async (requestBuilder: () => Promise<any>, retryOpts
     try {
       await requestBuilder()
     } catch (error) {
-      if (error.response) {
-        // If it's an axios error
-        if (!errorCodesNoRetry.includes(error.response.status)) {
-          // And a status code that is not excluded from retries, throw the error so that upload is retried
-          throw error
-        }
-      } else {
-        // If it's another error or an axios error let us retry just in case
-        // This will catch DNS resolution errors and connection timeouts
-        throw error
+      if (error.response && errorCodesNoRetry.includes(error.response.status)) {
+        // If it's an axios error with a status code that is excluded from retries, we bail to avoid retrying
+        bail(error)
+
+        return
       }
-      bail(error)
+      // Other cases are retried: other axios HTTP errors as well as
+      // non-axios errors such as DNS resolution errors and connection timeouts
+      throw error
     }
   }
 
