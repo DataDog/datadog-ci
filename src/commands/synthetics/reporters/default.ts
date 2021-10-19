3c2b35b0b85322fa4182fdb836add9490a408ed7
Etienne Lefebvre <etienne.lefebvre@datadoghq.com>
2021-10-19 11:54:13 +0200

    [s8s] implement `MainReporter` for `DefaultReporter`
    `DeafultReporter` is required to implement all `MainReporter` methods.

--- src/commands/synthetics/reporters/default.ts
+++ src/commands/synthetics/reporters/default.ts
@@ -8,9 +8,9 @@ import {
   ERRORS,
   ExecutionRule,
   LocationsMapping,
+  MainReporter,
   Operator,
   PollResult,
-  Reporter,
   Result,
   Step,
   Summary,
@@ -263,7 +263,7 @@ const getTestResultColor = (success: boolean, isNonBlocking: boolean) => {
   return chalk.bold.red
 }
 
-export class DefaultReporter implements Reporter {
+export class DefaultReporter implements MainReporter {
   private write: Writable['write']
 
   constructor({context}: {context: BaseContext}) {
