e734aecaf60d485be4705b6e045c8da43b3c48be
emil.gelman <emil.gelman@gmail.com>
2021-11-21 14:19:28 +0200

    allow overriding junit intake url with env

--- src/commands/junit/utils.ts
+++ src/commands/junit/utils.ts
@@ -1,5 +1,7 @@
 export const getBaseIntakeUrl = () => {
-  if (process.env.DATADOG_SITE || process.env.DD_SITE) {
+  if (process.env.DATADOG_JUNIT_INTAKE_URL) {
+    return process.env.DATADOG_JUNIT_INTAKE_URL
+  } else if (process.env.DATADOG_SITE || process.env.DD_SITE) {
     return `https://cireport-intake.${process.env.DATADOG_SITE || process.env.DD_SITE}`
   }
 
