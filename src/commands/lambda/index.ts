37eb4a303a1b358ab49c6329c7d180bad07b4db0
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2021-11-30 17:43:25 -0500

    add prompt mock

--- src/commands/lambda/index.ts
+++ src/commands/lambda/index.ts
@@ -1,4 +1,21 @@
+import { Command } from 'clipanion/lib/advanced'
+import { prompt } from 'inquirer'
 import {InstrumentCommand} from './instrument'
 import {UninstrumentCommand} from './uninstrument'
 
-module.exports = [InstrumentCommand, UninstrumentCommand]
+class LambdaCommand extends Command {
+  static paths = [[`lambda`]];
+  public async execute() {
+    prompt({
+      type: 'input',
+      name: 'test',
+      message: 'How would you define the datadog-ci package?',
+      default: "It's lit!",
+    }).then((answers) => {
+      console.log('\nNoted chief!');
+      console.log(JSON.stringify(answers, null, '  '));
+    });
+  }
+}
+
+module.exports = [InstrumentCommand, LambdaCommand, UninstrumentCommand]
