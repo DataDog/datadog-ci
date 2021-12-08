2e17a8d423dfc4faaa61d2663d1adea1e40183e8
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2021-12-08 15:00:27 -0500

    remove test command

--- src/commands/lambda/cli.ts
+++ src/commands/lambda/cli.ts
@@ -1,24 +1,4 @@
-import {bold, yellow} from 'chalk'
-import {Command} from 'clipanion/lib/advanced'
-import {isMissingAWSCredentials, isMissingDatadogEnvVars} from './functions/commons'
-
 import {InstrumentCommand} from './instrument'
-import {requestAWSCredentials, requestDatadogEnvVars} from './prompt'
 import {UninstrumentCommand} from './uninstrument'
 
-class LambdaCommand extends Command {
-  public async execute() {
-    if (isMissingAWSCredentials()) {
-      this.context.stdout.write(`${bold(yellow('[!]'))} AWS Credentials are missing, let's set them up!\n`)
-      await requestAWSCredentials()
-    }
-    if (isMissingDatadogEnvVars()) {
-      this.context.stdout.write(`${bold(yellow('[!]'))} Datadog Environment Variables are needed.\n`)
-      await requestDatadogEnvVars()
-    }
-  }
-}
-
-LambdaCommand.addPath('lambda')
-
-module.exports = [InstrumentCommand, LambdaCommand, UninstrumentCommand]
+module.exports = [InstrumentCommand, UninstrumentCommand]
