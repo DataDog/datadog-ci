0cc699978b313d0b75be5e2a88669a962a0c6dc2
Kevin YERAMIAN <kevin.yeramian@datadoghq.com>
2021-12-20 18:03:27 +0100

    [user-provided-git] test: simplify getUserGitMetadata tests

--- src/helpers/__tests__/user-provided-git.test.ts
+++ src/helpers/__tests__/user-provided-git.test.ts
@@ -15,17 +15,21 @@ import {
 import {getUserGitMetadata} from '../user-provided-git'
 
 describe('getUserGitMetadata', () => {
+  const DD_GIT_METADATA = {
+    DD_GIT_COMMIT_SHA: 'DD_GIT_COMMIT_SHA',
+    DD_GIT_REPOSITORY_URL: 'DD_GIT_REPOSITORY_URL',
+    DD_GIT_BRANCH: 'DD_GIT_BRANCH',
+    DD_GIT_COMMIT_MESSAGE: 'DD_GIT_COMMIT_MESSAGE',
+    DD_GIT_COMMIT_AUTHOR_NAME: 'DD_GIT_COMMIT_AUTHOR_NAME',
+    DD_GIT_COMMIT_AUTHOR_EMAIL: 'DD_GIT_COMMIT_AUTHOR_EMAIL',
+    DD_GIT_COMMIT_AUTHOR_DATE: 'DD_GIT_COMMIT_AUTHOR_DATE',
+    DD_GIT_COMMIT_COMMITTER_NAME: 'DD_GIT_COMMIT_COMMITTER_NAME',
+    DD_GIT_COMMIT_COMMITTER_EMAIL: 'DD_GIT_COMMIT_COMMITTER_EMAIL',
+    DD_GIT_COMMIT_COMMITTER_DATE: 'DD_GIT_COMMIT_COMMITTER_DATES',
+  }
+
   it('reads user defined git metadata successfully', () => {
-    process.env.DD_GIT_COMMIT_SHA = 'DD_GIT_COMMIT_SHA'
-    process.env.DD_GIT_REPOSITORY_URL = 'DD_GIT_REPOSITORY_URL'
-    process.env.DD_GIT_BRANCH = 'DD_GIT_BRANCH'
-    process.env.DD_GIT_COMMIT_MESSAGE = 'DD_GIT_COMMIT_MESSAGE'
-    process.env.DD_GIT_COMMIT_AUTHOR_NAME = 'DD_GIT_COMMIT_AUTHOR_NAME'
-    process.env.DD_GIT_COMMIT_AUTHOR_EMAIL = 'DD_GIT_COMMIT_AUTHOR_EMAIL'
-    process.env.DD_GIT_COMMIT_AUTHOR_DATE = 'DD_GIT_COMMIT_AUTHOR_DATE'
-    process.env.DD_GIT_COMMIT_COMMITTER_NAME = 'DD_GIT_COMMIT_COMMITTER_NAME'
-    process.env.DD_GIT_COMMIT_COMMITTER_EMAIL = 'DD_GIT_COMMIT_COMMITTER_EMAIL'
-    process.env.DD_GIT_COMMIT_COMMITTER_DATE = 'DD_GIT_COMMIT_COMMITTER_DATE'
+    process.env = {...DD_GIT_METADATA}
     const result = getUserGitMetadata()
     expect(result).toEqual({
       [GIT_REPOSITORY_URL]: 'DD_GIT_REPOSITORY_URL',
@@ -41,7 +45,7 @@ describe('getUserGitMetadata', () => {
     })
   })
   it('does not include empty values', () => {
-    delete process.env.DD_GIT_COMMIT_SHA
+    process.env = {...DD_GIT_METADATA, DD_GIT_COMMIT_SHA: undefined}
     const result = getUserGitMetadata()
     expect(result).toEqual({
       [GIT_REPOSITORY_URL]: 'DD_GIT_REPOSITORY_URL',
@@ -56,7 +60,7 @@ describe('getUserGitMetadata', () => {
     })
   })
   it('overwrites branch when tag is available', () => {
-    process.env.DD_GIT_TAG = 'DD_GIT_TAG'
+    process.env = {...DD_GIT_METADATA, DD_GIT_TAG: 'DD_GIT_TAG'}
     const result = getUserGitMetadata()
     expect(result).toEqual({
       [GIT_TAG]: 'DD_GIT_TAG',
@@ -71,17 +75,7 @@ describe('getUserGitMetadata', () => {
     })
   })
   it('returns an empty object if no user git is defined', () => {
-    delete process.env.DD_GIT_REPOSITORY_URL
-    delete process.env.DD_GIT_BRANCH
-    delete process.env.DD_GIT_TAG
-    delete process.env.DD_GIT_COMMIT_MESSAGE
-    delete process.env.DD_GIT_COMMIT_AUTHOR_NAME
-    delete process.env.DD_GIT_COMMIT_AUTHOR_EMAIL
-    delete process.env.DD_GIT_COMMIT_AUTHOR_DATE
-    delete process.env.DD_GIT_COMMIT_COMMITTER_NAME
-    delete process.env.DD_GIT_COMMIT_COMMITTER_EMAIL
-    delete process.env.DD_GIT_COMMIT_COMMITTER_DATE
-
+    process.env = {}
     const result = getUserGitMetadata()
     expect(result).toEqual({})
   })
