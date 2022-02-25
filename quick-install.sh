35b289c6860ab0ac75bdae9380602c12751097da
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2022-02-25 11:03:31 +0100

    Standalone binary for `datadog-ci` (#485)

---
+++ quick-install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+REPO_BASE_URL="https://github.com/DataDog/datadog-ci"
+INSTALL_PATH="/usr/local/bin/datadog-ci"
+
+function download {
+    detect_os
+    curl -L --fail "$REPO_BASE_URL/releases/latest/download/datadog-ci-$os_suffix" --output $INSTALL_PATH
+    chmod +x $INSTALL_PATH
+}
+
+function detect_os {
+    case "$OSTYPE" in
+        linux*)   os_suffix="linux" ;;
+        darwin*)  os_suffix="macos" ;;
+        *)        echo "unsupported OS: $OSTYPE"; exit 1 ;;
+    esac
+}
+
+if [ ! -x $INSTALL_PATH ]; then download ; fi
