fd5e57ae4e8fceb32c4f51a2d6b09ca85b291b9c
Benoit Ruiz <benoit.ruiz@datadoghq.com>
2022-05-11 18:12:55 +0200

    [synthetics] Adapt default reporter Jest tests

--- src/commands/synthetics/__tests__/reporters/default.test.ts
+++ src/commands/synthetics/__tests__/reporters/default.test.ts
@@ -140,42 +140,42 @@ describe('Default reporter', () => {
       writeMock.mockClear()
     })
 
-    const bGreen = (s: string) => chalk.bold.green(s)
-    const bYellow = (s: string) => chalk.bold.yellow(s)
-    const bRed = (s: string) => chalk.bold.red(s)
-    const bDim = (s: string) => chalk.bold.dim(s)
+    const b = chalk.bold
+    const bGreen = chalk.bold.green
+    const bYellow = chalk.bold.yellow
+    const bRed = chalk.bold.red
+    const bDim = chalk.bold.dim
 
-    const testLabelSuccess = (publicId: string) => `${bGreen('✓')} [${bDim(publicId)}] | ${bGreen('Test name')}`
-    const testLabelFailed = (publicId: string) => `${bRed('✖')} [${bDim(publicId)}] | ${bRed('Test name')}`
-    const testLabelFailedNonBlocking = (publicId: string) =>
-      `${bYellow('✖')} [${bDim(publicId)}] | ${bYellow('Test name')}`
-    const locationSuccess = chalk.bold.green(`  ${bGreen('✓')} location: ${chalk.bold(mockLocation.display_name)}`)
-    const locationFailed = chalk.bold.red(`  ${bRed('✖')} location: ${chalk.bold(mockLocation.display_name)}`)
-    const locationFailedNonBlocking = chalk.bold.yellow(
-      `  ${bYellow('✖')} location: ${chalk.bold(mockLocation.display_name)}`
-    )
+    const firstLineSuccess = (publicId: string) =>
+      `${bGreen('✓')} [${bDim(publicId)}] ${b('Test name')} - ${bGreen(`location: ${b(mockLocation.display_name)}`)}`
+    const firstLineFailed = (publicId: string) =>
+      `${bRed('✖')} [${bRed('blocking')}] [${bDim(publicId)}] ${b('Test name')} - ${bRed(
+        `location: ${b(mockLocation.display_name)}`
+      )}`
+    const firstLineFailedNonBlocking = (publicId: string) =>
+      `${bYellow('✖')} [${bYellow('non-blocking')}] [${bDim(publicId)}] ${b('Test name')} - ${bYellow(
+        `location: ${b(mockLocation.display_name)}`
+      )}`
 
     const durationWithResultUrl = (resultId: string) =>
-      `    ⎋ total duration: 123 ms - result url: ${chalk.dim.cyan(
+      `  ⎋ Total duration: 123 ms - Result URL: ${chalk.dim.cyan(
         `https://app.datadoghq.com/synthetics/details/aaa-aaa-aaa?resultId=${resultId}&from_ci=true`
       )} `
 
-    const apiResultSuccess = `    ${bGreen('✓')} ${bGreen(chalk.bold('GET') + ' - http://fake.url')}`
-    const apiResultFailed = `    ${bRed('✖')} ${bRed('[blocking]')} ${bRed(chalk.bold('GET') + ' - http://fake.url')}`
-    const apiResultFailedNonBlocking = `    ${bYellow('✖')} ${bYellow('[non-blocking]')} ${bYellow(
-      chalk.bold('GET') + ' - http://fake.url'
-    )}`
+    const apiResultSuccess = `  ${bGreen('✓')} ${bGreen(b('GET') + ' - http://fake.url')}`
+    const apiResultFailed = `  ${bRed('✖')} ${bRed(b('GET') + ' - http://fake.url')}`
+    const apiResultFailedNonBlocking = `  ${bYellow('✖')} ${bYellow(b('GET') + ' - http://fake.url')}`
 
     const apiResultFailedAssertions = bRed(
       [
-        '    - Assertion(s) failed:',
-        `      ▶ responseTime should be less than ${chalk.underline('1000')}. Actual: ${chalk.underline('1234')}`,
+        '  - Assertion(s) failed:',
+        `    ▶ responseTime should be less than ${chalk.underline('1000')}. Actual: ${chalk.underline('1234')}`,
       ].join('\n')
     )
     const apiResultFailedNonBlockingAssertions = bYellow(
       [
-        '    - Assertion(s) failed:',
-        `      ▶ responseTime should be less than ${chalk.underline('1000')}. Actual: ${chalk.underline('1234')}`,
+        '  - Assertion(s) failed:',
+        `    ▶ responseTime should be less than ${chalk.underline('1000')}. Actual: ${chalk.underline('1234')}`,
       ].join('\n')
     )
 
@@ -208,13 +208,9 @@ describe('Default reporter', () => {
     const cases = [
       {
         description: '1 API test, 1 location, 1 result: success',
-        expectedOutput: [
-          testLabelSuccess('aaa-aaa-aaa'),
-          locationSuccess,
-          durationWithResultUrl('1'),
-          apiResultSuccess,
-          '\n',
-        ].join('\n'),
+        expectedOutput: [firstLineSuccess('aaa-aaa-aaa'), durationWithResultUrl('1'), apiResultSuccess, '\n'].join(
+          '\n'
+        ),
         fixtures: {
           baseUrl: baseUrlFixture,
           failOnCriticalErrors: false,
@@ -227,17 +223,16 @@ describe('Default reporter', () => {
       {
         description: '1 API test (blocking), 1 location, 3 results: success, failed non-blocking, failed',
         expectedOutput: [
-          testLabelFailed('aaa-aaa-aaa'),
-          locationSuccess,
+          firstLineSuccess('aaa-aaa-aaa'),
           durationWithResultUrl('1'),
           apiResultSuccess,
           '',
-          locationFailedNonBlocking,
+          firstLineFailedNonBlocking('aaa-aaa-aaa'),
           durationWithResultUrl('2'),
           apiResultFailedNonBlocking,
           apiResultFailedNonBlockingAssertions,
           '',
-          locationFailed,
+          firstLineFailed('aaa-aaa-aaa'),
           durationWithResultUrl('3'),
           apiResultFailed,
           apiResultFailedAssertions,
@@ -259,17 +254,16 @@ describe('Default reporter', () => {
       {
         description: '1 API test (non-blocking), 1 location, 3 results: success, failed non-blocking, failed',
         expectedOutput: [
-          testLabelFailedNonBlocking('aaa-aaa-aaa'),
-          locationSuccess,
+          firstLineSuccess('aaa-aaa-aaa'),
           durationWithResultUrl('1'),
           apiResultSuccess,
           '',
-          locationFailedNonBlocking,
+          firstLineFailedNonBlocking('aaa-aaa-aaa'),
           durationWithResultUrl('2'),
           apiResultFailedNonBlocking,
           apiResultFailedNonBlockingAssertions,
           '',
-          locationFailedNonBlocking,
+          firstLineFailedNonBlocking('aaa-aaa-aaa'),
           durationWithResultUrl('3'),
           apiResultFailedNonBlocking,
           apiResultFailedNonBlockingAssertions,
@@ -302,22 +296,19 @@ describe('Default reporter', () => {
       writeMock.mockClear()
     })
 
-    const testsLabel = chalk.bold('Tests summary:')
-    const resultsLabel = chalk.bold('Results summary:')
-    const testsFound = (n: number) => chalk.green(`${chalk.bold(n)} found`)
+    const resultsLabel = chalk.bold('Run summary:')
     const testsNotFound = (n: number, publicIds: string[]) =>
-      `${chalk.yellow(`${chalk.bold(n)} not found`)} ${chalk.gray(`(${publicIds.join(', ')})`)}`
-    const testsSkipped = (n: number) => `${chalk.bold(n)} skipped`
+      `${chalk.yellow(`${chalk.bold(n)} test${n !== 1 ? 's' : ''} not found`)} ${chalk.gray(
+        `(${publicIds.join(', ')})`
+      )}`
     const passed = (n: number) => chalk.green(`${chalk.bold(n)} passed`)
     const failed = (n: number) => chalk.red(`${chalk.bold(n)} failed`)
     const failedNonBlocking = (n: number) => chalk.yellow(`${chalk.bold(n)} failed (non-blocking)`)
+    const skipped = (n: number) => `${chalk.bold(n)} skipped`
     const timedOut = (n: number) => chalk.yellow(`${chalk.bold(n)} timed out`)
     const criticaErrors = (n: number) => chalk.red(`${chalk.bold(n)} critical errors`)
 
-    const baseSummary: Summary = {
-      ...createSummary(),
-      testsFound: new Set(['aaa-aaa-aaa']),
-    }
+    const baseSummary: Summary = createSummary()
 
     const complexSummary: Summary = {
       criticalErrors: 2,
@@ -325,36 +316,31 @@ describe('Default reporter', () => {
       failedNonBlocking: 3,
       passed: 2,
       skipped: 1,
-      testsFound: new Set(['aaa-aaa-aaa', 'bbb-bbb-bbb']),
-      testsNotFound: new Set(['ccc-ccc-ccc']),
+      testsNotFound: new Set(['ccc-ccc-ccc', 'ddd-ddd-ddd']),
       timedOut: 1,
     }
 
     const cases: {description: string; expectedOutput: string; summary: Summary}[] = [
       {
         description: 'Simple case with 1 test with 1 result (passed)',
-        expectedOutput: [
-          `${testsLabel} ${testsFound(1)}`,
-          `${resultsLabel} ${passed(1)}, ${failed(0)}, ${failedNonBlocking(0)}`,
-          '',
-        ].join('\n'),
+        expectedOutput: `${resultsLabel} ${passed(1)}, ${failed(0)}, ${failedNonBlocking(0)}\n\n`,
         summary: {...baseSummary, passed: 1},
       },
       {
         description: 'Complex case with all the tests and results outcomes possible',
         expectedOutput: [
-          `${testsLabel} ${testsFound(2)}, ${testsSkipped(1)}, ${testsNotFound(1, ['ccc-ccc-ccc'])}`,
-          `${resultsLabel} ${passed(2)}, ${failed(1)}, ${failedNonBlocking(3)} (${timedOut(1)}, ${criticaErrors(2)})`,
-          '',
+          testsNotFound(2, ['ccc-ccc-ccc', 'ddd-ddd-ddd']),
+          `${resultsLabel} ${passed(2)}, ${failed(1)}, ${failedNonBlocking(3)}, ${skipped(1)} (${timedOut(
+            1
+          )}, ${criticaErrors(2)})\n\n`,
         ].join('\n'),
         summary: complexSummary,
       },
       {
         description: 'Case where some outcomes are empty or missing',
         expectedOutput: [
-          `${testsLabel} ${testsFound(1)}, ${testsNotFound(1, ['bbb-bbb-bbb'])}`,
-          `${resultsLabel} ${passed(3)}, ${failed(0)}, ${failedNonBlocking(1)} (${criticaErrors(1)})`,
-          '',
+          testsNotFound(1, ['bbb-bbb-bbb']),
+          `${resultsLabel} ${passed(3)}, ${failed(0)}, ${failedNonBlocking(1)} (${criticaErrors(1)})\n\n`,
         ].join('\n'),
         summary: {
           ...baseSummary,
