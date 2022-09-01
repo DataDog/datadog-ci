b8799b1b76989af7d7981f92b5664c7d39ce9c1b
Kevin YERAMIAN <kevin.yeramian@datadoghq.com>
2022-09-01 16:02:47 +0200

    Synthetics-mobile: Add getMD5HashFromFileBuffer

---
+++ src/commands/synthetics/mobile.ts
@@ -0,0 +1,7 @@
+import * as crypto from 'crypto'
+
+export const getMD5HashFromFileBuffer = async (fileBuffer: Buffer): Promise<string> => {
+  const hash = crypto.createHash('md5').update(fileBuffer).digest('base64')
+
+  return hash
+}
