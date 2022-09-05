61de467a5b6d0832d57afa2bc8ff1a997c7fae33
Kevin YERAMIAN <kevin.yeramian@datadoghq.com>
2022-09-05 13:59:07 +0200

    Remove tmp-promise and update test

--- src/commands/synthetics/__tests__/mobile.test.ts
+++ src/commands/synthetics/__tests__/mobile.test.ts
@@ -1,16 +1,7 @@
-import {promises as fs} from 'fs'
-import * as path from 'path'
-
 import * as mobile from '../mobile'
-
-// tslint:disable-next-line:no-var-requires
-const tmp = require('tmp-promise')
 
 describe('getMD5HashFromFileBuffer', () => {
   test('correctly compute md5 of a file', async () => {
-    const dir = (await tmp.dir({mode: 0o755, unsafeCleanup: true})).path
-    await fs.writeFile(path.join(dir, 'file.txt'), 'Compute md5')
-    const fileBuffer = await fs.readFile(path.join(dir, 'file.txt'))
-    expect(await mobile.getMD5HashFromFileBuffer(fileBuffer)).toBe('odk1EOlpz16oPIgnco2nfg==')
+    expect(await mobile.getMD5HashFromFileBuffer(Buffer.from('Compute md5'))).toBe('odk1EOlpz16oPIgnco2nfg==')
   })
 })
