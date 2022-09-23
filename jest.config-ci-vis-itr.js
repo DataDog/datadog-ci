082a2e490156f2f679d35d918d9cf108922dc46e
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2022-09-23 15:11:38 +0200

    add new itr workflow

---
+++ jest.config-ci-vis-itr.js
@@ -0,0 +1,12 @@
+module.exports = {
+  clearMocks: true,
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  roots: ['src'],
+  setupFilesAfterEnv: ['jest-matcher-specific-error']
+}
