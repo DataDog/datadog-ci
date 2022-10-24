0257f26695335525e9a1032709a485ab7b169413
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-10-24 17:59:31 +0200

    Get properties from plist

--- src/helpers/__tests__/plist.test.ts
+++ src/helpers/__tests__/plist.test.ts
@@ -7,7 +7,33 @@ describe('plist util', () => {
       expect(plist.getContent()).toMatchSnapshot()
     })
 
-    it('throws an error if the file does not exist', () => {})
-    it('throws an error if the file is not correctly formatted', () => {})
+    it('returns the value for a property', () => {
+      const plist = parsePlist('src/helpers/__tests__/plist-fixtures/Info.plist')
+      expect(plist.getPropertyValue('CFBundleShortVersionString')).toBe('1.0.4')
+      expect(plist.getPropertyValue('CFBundleVersion')).toBe(12)
+    })
+
+    it('throws an error if a property does not exist', () => {
+      const plist = parsePlist('src/helpers/__tests__/plist-fixtures/Info.plist')
+      expect(() => plist.getPropertyValue('NonExistingValue')).toThrowError('Property not found')
+    })
+
+    it('throws an error if a property is not a string', () => {
+      const plist = parsePlist('src/helpers/__tests__/plist-fixtures/Info.plist')
+      expect(() => plist.getPropertyValue('CFBundleURLTypes')).toThrowError(
+        'Property is not a string, this is not supported yet'
+      )
+    })
+
+    it('throws an error if the file does not exist', () => {
+      expect(() => parsePlist('non-existing-file')).toThrowError(
+        "ENOENT: no such file or directory, open 'non-existing-file'"
+      )
+    })
+    it('throws an error if the file is not correctly formatted', () => {
+      expect(() => parsePlist('src/helpers/__tests__/plist-fixtures/BadlyFormatted.plist')).toThrowError(
+        "Expected closing tag 'string' (opened in line 7, col 2) instead of closing tag 'dict'."
+      )
+    })
   })
 })
--- src/helpers/plist.ts
+++ src/helpers/plist.ts
@@ -2,13 +2,73 @@ import {readFileSync} from 'fs'
 
 import {XMLParser, XMLValidator} from 'fast-xml-parser'
 
+type PlistNode = {[tagName: string]: [{[valueType: string]: string | number | PlistNode}]}
+
 class PlistContent {
-  private content: Record<string, unknown>
-  constructor(content: Record<string, unknown>) {
+  private content: PlistNode[]
+  constructor(content: PlistNode[]) {
     this.content = content
   }
 
   public getContent = () => this.content
+
+  /**
+   *
+   * @param propertyName
+   * @returns
+   */
+  public getPropertyValue = (propertyName: string): string | number => {
+    const propertyNodeIndex = this.content.findIndex((node) => {
+      if (!this.isKeyNode(node)) {
+        return false
+      }
+
+      return node.key[0]['#text'] === propertyName
+    })
+
+    if (propertyNodeIndex === -1) {
+      throw new Error('Property not found')
+    }
+
+    const valueNode = this.content[propertyNodeIndex + 1]
+    if (!this.isStringNode(valueNode)) {
+      throw new Error('Property is not a string, this is not supported yet')
+    }
+
+    return valueNode.string[0]['#text']
+  }
+
+  /**
+   * A key entry will be turned into element of the content looking like this:
+   * ```
+   * {
+   *   "key": [
+   *     {
+   *       "#text": "CFBundleDevelopmentRegion",
+   *     },
+   *   ],
+   * }
+   * ```
+   */
+  private isKeyNode = (node: PlistNode): node is {key: [{'#text': string}]} => {
+    return !!node.key
+  }
+
+  /**
+   * A string entry will be turned into element of the content looking like this:
+   * ```
+   * {
+   *   "string": [
+   *     {
+   *       "#text": "1.0.3",
+   *     },
+   *   ],
+   * }
+   * ```
+   */
+  private isStringNode = (node: PlistNode): node is {string: [{'#text': string | number}]} => {
+    return !!node.string
+  }
 }
 
 export const parsePlist = (plistPath: string): PlistContent => {
@@ -22,7 +82,7 @@ export const parsePlist = (plistPath: string): PlistContent => {
   const xmlParser = new XMLParser({preserveOrder: true})
   const [_xmlHeader, plistDeclaration] = xmlParser.parse(xmlFileContentString)
 
-  const plistContent = plistDeclaration.plist[0].dict
+  const plistContent = plistDeclaration.plist[0].dict as PlistNode[]
 
   return new PlistContent(plistContent)
 }
