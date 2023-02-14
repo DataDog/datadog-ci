afd72964b8793e24425d652b044778595c33a23c
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2023-02-14 13:03:22 +0100

    SARIF command

---
+++ src/commands/sarif/interfaces.ts
@@ -0,0 +1,14 @@
+import {Writable} from 'stream'
+
+import {AxiosPromise, AxiosResponse} from 'axios'
+
+import {SpanTags} from '../../helpers/interfaces'
+
+export interface Payload {
+    reportPath: string
+    spanTags: SpanTags
+}
+
+export interface APIHelper {
+    uploadSarifReport(sarifReport: Payload, write: Writable['write']): AxiosPromise<AxiosResponse>
+}
