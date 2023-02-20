9ad07bed2004a1566bd62b35e5cab8c2cf6e8a46
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2023-02-20 14:24:55 +0100

    Apply PR feedback

--- src/commands/sarif/__tests__/api.test.ts
+++
@@ -1,7 +0,0 @@
-import {getSafeFileName} from '../api'
-
-describe('getSafeFileName', () => {
-  test('filters unsafe characters out', () => {
-    expect(getSafeFileName('http://gitlab.com/-/pipelines/12345')).toEqual('http___gitlab_com___pipelines_12345')
-  })
-})
