38d730aeefed280eabef1b35a8c35e9092fa4a51
Loic Raucy <loic.raucy@datadoghq.com>
2023-02-20 20:02:39 +0100

    Adapt git-metadata upload to synchronize local git repository.

---
+++ src/commands/git-metadata/__tests__/gitdb.test.ts
@@ -0,0 +1,650 @@
+import child_process from 'child_process'
+import {default as axios} from 'axios'
+import fs from 'fs'
+import os from 'os'
+import {newSimpleGit} from '../git'
+import {Logger, LogLevel} from '../utils'
+import * as simpleGit from 'simple-git'
+
+import {uploadToGitDB} from '../gitdb'
+import {getRequestBuilder} from '../../../helpers/utils'
+
+let gitInstance: simpleGit.SimpleGit | undefined
+
+const getGitInstance = async () => {
+  if (gitInstance === undefined) {
+    gitInstance = await (newSimpleGit())
+  }
+  return gitInstance
+}
+
+describe('gitdb', () => {
+
+  const tmpdir = os.tmpdir()
+
+  const temporaryPackFile = `${tmpdir}/1000-87ce64f636853fbebc05edfcefe9cccc28a7968b.pack`
+  const secondTemporaryPackFile = `${tmpdir}/1000-cc424c261da5e261b76d982d5d361a023556e2aa.pack`
+
+  beforeAll(() => {
+    process.env.DD_API_KEY = 'api-key'
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.1)
+    fs.writeFileSync(temporaryPackFile, '')
+    fs.writeFileSync(secondTemporaryPackFile, '')
+  })
+
+  afterAll(() => {
+    delete process.env.DD_API_KEY
+    jest.spyOn(global.Math, 'random').mockRestore()
+    fs.unlinkSync(temporaryPackFile)
+    fs.unlinkSync(secondTemporaryPackFile)
+  })
+
+  const logger = new Logger(_ => {}, LogLevel.INFO)
+  const request = getRequestBuilder({
+    apiKey: 'api-key',
+    baseUrl: 'https://api.datadoghq.com',
+  })
+  const testError = new Error('call failed')
+
+  type MockParam<I, O> = {
+    input: I | undefined
+    output: O | Error
+  }
+  interface MockParams {
+    getRemotes: MockParam<void, Object>[]
+    log: MockParam<string[], Object>[]
+    raw: MockParam<string[], string>[]
+    execSync: MockParam<string, Buffer>[]
+    axios: MockParam<{
+      url: string
+      data: object | undefined
+    }, object>[]
+  }
+
+  class MockAll {
+    public simpleGit: {
+      getRemotes: jest.Mock
+      log: jest.Mock
+      raw: jest.Mock
+    }
+    public execSync: jest.Mock
+    public axios: jest.Mock
+    public expectCalls() {
+      this.getRemotesMetExpectations()
+      this.logMetExpectations()
+      this.rawMetExpectations()
+      this.execSyncMetExpectations()
+      this.axiosMetExpectations()
+    }
+
+    private getRemotesMetExpectations: () => void
+    private logMetExpectations: () => void
+    private rawMetExpectations: () => void
+    private execSyncMetExpectations: () => void
+    private axiosMetExpectations: () => void
+
+    private axiosCalls: {
+      url: string,
+      data: string | undefined
+    }[]
+
+    constructor(params: MockParams) {
+      this.simpleGit = {
+        getRemotes: jest.fn(),
+        log: jest.fn(),
+        raw: jest.fn(),
+      }
+      // call spyOn on these two mocks to make sure the underlying implementation is never called
+      // as the default behavior of spyOn is to actually call the initial implem if not overridden
+      this.execSync = jest.spyOn(child_process, 'execSync').mockImplementation(() => '') as jest.Mock
+      this.axios = jest.spyOn(axios, 'create').mockImplementation(() => ((_: any) => {}) as any) as jest.Mock
+
+      const initMockWithParams = <I, O>(mock: jest.Mock, params: MockParam<I, O>[], promise: boolean) => {
+        params.forEach(param => {
+          if (param.output instanceof Error) {
+            mock = mock.mockImplementationOnce((..._: any) => {throw param.output})
+          } else {
+            if (promise) {
+              mock = mock.mockResolvedValueOnce(param.output)
+            } else {
+              mock = mock.mockReturnValueOnce(param.output)
+            }
+          }
+        })
+        return () => {
+          expect(mock.mock.calls).toHaveLength(params.length)
+          params.forEach((param, i) => {
+            if (param.input !== undefined) {
+              expect(mock.mock.calls[i][0]).toStrictEqual(param.input)
+            }
+          })
+        }
+      }
+
+      this.getRemotesMetExpectations = initMockWithParams(this.simpleGit.getRemotes, params.getRemotes, true)
+      this.logMetExpectations = initMockWithParams(this.simpleGit.log, params.log, true)
+      this.rawMetExpectations = initMockWithParams(this.simpleGit.raw, params.raw, true)
+      this.execSyncMetExpectations = initMockWithParams(this.execSync, params.execSync, false)
+
+      this.axiosCalls = []
+
+      // custom way of handling axios
+      params.axios.forEach(param => {
+        this.axios = this.axios.mockImplementationOnce(
+          () => (request: any) => {
+            this.axiosCalls.push({url: request.url, data: request.data})
+            if (param.output instanceof Error) {
+              throw param.output
+            }
+            return param.output
+          }
+        )
+      })
+      this.axiosMetExpectations = () => {
+        expect(this.axios.mock.calls).toHaveLength(params.axios.length)
+        params.axios.forEach((param, i) => {
+          if (param.input !== undefined) {
+            expect(this.axiosCalls[i].url).toBe(param.input.url)
+            if (param.input.data !== undefined) {
+              const data = this.axiosCalls[i].data as string
+              expect(JSON.parse(data)).toStrictEqual(param.input.data)
+            }
+          }
+        })
+      }
+    }
+  }
+
+  test('should not work when remote is not present', async () => {
+    const mocks = new MockAll({
+      getRemotes: [{input: undefined, output: testError}],
+      log: [],
+      raw: [],
+      execSync: [],
+      axios: [],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).rejects.toThrow(testError)
+    try {
+      await upload
+    } catch (e) {
+    }
+    mocks.expectCalls()
+  })
+
+  test('should send packfiles', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [{
+        input: ['rev-list', '--objects', '--no-object-names', '--filter=blob:none', '--since="1 month ago"', 'HEAD'],
+        output: '87ce64f636853fbebc05edfcefe9cccc28a7968b\ncc424c261da5e261b76d982d5d361a023556e2aa\n'
+      }],
+      execSync: [{
+        input: `git pack-objects --compression=9 --max-pack-size=3m ${tmpdir}/1000`,
+        output: Buffer.from('87ce64f636853fbebc05edfcefe9cccc28a7968b\ncc424c261da5e261b76d982d5d361a023556e2aa\n'),
+      }],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: {data: {data: []}},
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/packfile',
+            data: undefined,
+          },
+          output: {},
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/packfile',
+            data: undefined,
+          },
+          output: {},
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).resolves
+    await upload
+    mocks.expectCalls()
+  })
+
+  test('should omit known commits', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [{
+        input: ['rev-list', '--objects', '--no-object-names', '--filter=blob:none', '--since="1 month ago"', 'HEAD',
+          '^87ce64f636853fbebc05edfcefe9cccc28a7968b',
+        ],
+        output: 'cc424c261da5e261b76d982d5d361a023556e2aa\n'
+      }],
+      execSync: [{
+        input: `git pack-objects --compression=9 --max-pack-size=3m ${tmpdir}/1000`,
+        output: Buffer.from('cc424c261da5e261b76d982d5d361a023556e2aa\n'),
+      }],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: {
+            data: {
+              data: [{
+                id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                type: 'commit',
+              }]
+            }
+          },
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/packfile',
+            data: undefined,
+          },
+          output: {},
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).resolves
+    await upload
+    mocks.expectCalls()
+  })
+
+  test('retries http requests', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [{
+        input: ['rev-list', '--objects', '--no-object-names', '--filter=blob:none', '--since="1 month ago"', 'HEAD',
+          '^87ce64f636853fbebc05edfcefe9cccc28a7968b',
+        ],
+        output: 'cc424c261da5e261b76d982d5d361a023556e2aa\n'
+      }],
+      execSync: [{
+        input: `git pack-objects --compression=9 --max-pack-size=3m ${tmpdir}/1000`,
+        output: Buffer.from('cc424c261da5e261b76d982d5d361a023556e2aa\n'),
+      }],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: new Error('http error'),
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: {
+            data: {
+              data: [{
+                id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                type: 'commit',
+              }]
+            }
+          },
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/packfile',
+            data: undefined,
+          },
+          output: {},
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).resolves
+    await upload
+    mocks.expectCalls()
+  })
+
+  test('fails after 3 http requests', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [],
+      execSync: [],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: new Error('http error'),
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: new Error('http error'),
+        },
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: new Error('http error'),
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).rejects
+    try {
+      await upload
+    } catch (e) {
+    }
+    mocks.expectCalls()
+  })
+
+  test('fail immediately if returned format is incorrect', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [],
+      execSync: [],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: {
+            data: {
+              data: [
+                {
+                  type: 'commit',
+
+                },
+                {
+                  type: 'commit',
+                },
+              ]
+            }
+          },
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).rejects
+    try {
+      await upload
+    } catch (e) {
+    }
+    mocks.expectCalls()
+  })
+
+  test('all commits are known, no packfile upload', async () => {
+    const mocks = new MockAll({
+      getRemotes: [
+        {
+          input: undefined,
+          output: [
+            {name: 'origin', refs: {push: 'https://github.com/DataDog/datadog-ci'}},
+          ]
+        },
+      ],
+      log: [{
+        input: ['-n 1000', '--since="1 month ago"'],
+        output: {
+          all: [{
+            hash: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+          }, {
+            hash: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+          }],
+        }
+      }],
+      raw: [{
+        input: ['rev-list', '--objects', '--no-object-names', '--filter=blob:none', '--since="1 month ago"', 'HEAD',
+          '^87ce64f636853fbebc05edfcefe9cccc28a7968b', '^cc424c261da5e261b76d982d5d361a023556e2aa',
+        ],
+        output: '\n'
+      }],
+      execSync: [],
+      axios: [
+        {
+          input: {
+            url: '/api/v2/git/repository/search_commits',
+            data: {
+              meta: {
+                repository_url: 'https://github.com/DataDog/datadog-ci',
+              },
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ],
+            },
+          },
+          output: {
+            data: {
+              data: [
+                {
+                  id: '87ce64f636853fbebc05edfcefe9cccc28a7968b',
+                  type: 'commit',
+
+                },
+                {
+                  id: 'cc424c261da5e261b76d982d5d361a023556e2aa',
+                  type: 'commit',
+                },
+              ]
+            }
+          },
+        },
+      ],
+    })
+    const upload = uploadToGitDB(logger, request, mocks.simpleGit as any, false)
+    expect(upload).resolves
+    await upload
+    mocks.expectCalls()
+  })
+})
---
+++ src/commands/git-metadata/gitdb.ts
@@ -0,0 +1,247 @@
+import * as simpleGit from 'simple-git'
+import FormData from 'form-data'
+import os from 'os'
+import fs from 'fs'
+import path from 'path'
+import child_process from 'child_process'
+import {AxiosResponse} from 'axios'
+
+import {gitRemote} from './git'
+import {Logger} from './utils'
+import {RequestBuilder} from '../../helpers/interfaces'
+import {retryRequest} from '../../helpers/retry'
+
+const API_TIMEOUT = 15000
+
+export const uploadToGitDB = async (log: Logger, request: RequestBuilder, git: simpleGit.SimpleGit, dryRun: boolean) => {
+  let repoURL
+  try {
+    repoURL = await getRepoURL(git)
+    log.debug(`Syncing repository ${repoURL}`)
+  } catch (err) {
+    log.warn(`Failed getting repository URL: ${err}`)
+    throw err
+  }
+
+  let latestCommits
+  try {
+    latestCommits = await getLatestLocalCommits(git)
+    if (latestCommits.length === 0) {
+      log.debug('No local commits found.')
+      return
+    }
+    log.debug(`${latestCommits.length} commits found, asking GitDB which ones are missing.`)
+  } catch (err) {
+    log.warn(`Failed getting local commits: ${err}`)
+    throw err
+  }
+
+  let commitsToExclude
+  try {
+    commitsToExclude = await getKnownCommits(log, request, repoURL, latestCommits)
+    log.debug(`${commitsToExclude.length} commits already in GitDB.`)
+  } catch (err) {
+    log.warn(`Failed getting commits to exclude: ${err}`)
+    throw err
+  }
+
+  let commitsToUpload
+  try {
+    commitsToUpload = await getCommitsToUpload(git, commitsToExclude)
+    log.debug(`${commitsToUpload.length} commits to upload.`)
+  } catch (err) {
+    log.warn(`Failed getting commits to upload: ${err}`)
+    throw err
+  }
+
+  let packfiles
+  try {
+    packfiles = generatePackFilesForCommits(log, commitsToUpload)
+    log.debug(`${packfiles.length} packfiles generated.`)
+  } catch (err) {
+    log.warn(`Failed generating packfiles: ${err}`)
+    throw err
+  }
+
+  if (dryRun) {
+    log.debug(`Dry-run enabled, not uploading anything.`)
+    return
+  }
+  log.debug(`Uploading packfiles...`)
+  try {
+    await uploadPackfiles(log, request, repoURL, latestCommits[0], packfiles)
+    log.debug(`Successfully uploaded packfiles.`)
+  } catch (err) {
+    log.warn(`Failed to upload packfiles: ${err}`)
+    throw err
+  }
+}
+
+const getRepoURL = gitRemote
+
+const getLatestLocalCommits = async (git: simpleGit.SimpleGit) => {
+  const logResult = await git.log(['-n 1000', '--since="1 month ago"'])
+  return logResult.all.map(c => c.hash)
+}
+
+// getKnownCommits asks the backend which of the given commits are already known
+const getKnownCommits = async (log: Logger, request: RequestBuilder, repoURL: string, latestCommits: string[]) => {
+
+  interface SearchCommitResponse {
+    data: Commit[]
+  }
+
+  interface Commit {
+    type: string
+    id: string
+  }
+
+  const localCommitData = JSON.stringify({
+    meta: {
+      repository_url: repoURL,
+    },
+    data: latestCommits.map(commit => ({
+      id: commit,
+      type: 'commit'
+    }))
+  })
+  const response = await runRequest(log, "search_commits", () => request({
+    url: '/api/v2/git/repository/search_commits',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: localCommitData,
+    method: 'POST',
+    timeout: API_TIMEOUT,
+  }))
+  const commits = (response.data as SearchCommitResponse)
+  if (!commits || commits.data === undefined) {
+    throw new Error(`Invalid API response: ${response}`)
+  }
+  return commits.data.map(c => {
+    if (c.type !== 'commit') {
+      throw new Error('Invalid commit type response')
+    }
+    return sanitizeCommit(c.id)
+  })
+}
+
+const sanitizeCommit = (sha: string) => {
+  const isValidSha = (sha: string) => /[0-9a-f]{40}/.test(sha)
+
+  const sanitizedCommit = sha.replace(/[^0-9a-f]+/g, '')
+  if (sanitizedCommit !== sha) {
+    throw new Error(`Invalid commit format: ${sha} (different from sanitized ${sanitizedCommit})`)
+  }
+  if (!isValidSha(sanitizedCommit)) {
+    throw new Error(`Invalid commit format: ${sanitizedCommit}`)
+  }
+  return sanitizedCommit
+}
+
+const getCommitsToUpload = async (git: simpleGit.SimpleGit, commitsToExclude: string[]) => {
+  const rawResponse = await git.raw([
+    'rev-list',
+    '--objects',
+    '--no-object-names',
+    '--filter=blob:none',
+    '--since="1 month ago"',
+    'HEAD',
+  ].concat(commitsToExclude.map(sha => '^' + sha)))
+  const commitsToInclude = rawResponse.split('\n').filter(c => c !== "")
+  return commitsToInclude
+}
+
+// need to transform into async here.
+const generatePackFilesForCommits = (log: Logger, commits: string[]) => {
+  if (commits.length <= 0) {
+    return []
+  }
+
+  const generatePackfiles = (path: string) => {
+    const packObjectResults = child_process.execSync(`git pack-objects --compression=9 --max-pack-size=3m ${path}`, {
+      input: commits.join('\n')
+    }).toString().split('\n').filter(sha => sha).map(sha => `${path}-${sha}.pack`)
+    return packObjectResults
+  }
+
+  // Try using tmp folder first:
+  try {
+    const tmpFolder = os.tmpdir()
+    const randomPrefix = String(Math.floor(Math.random() * 10000))
+    const tmpPath = path.join(tmpFolder, randomPrefix)
+    return generatePackfiles(tmpPath)
+  } catch (err) {
+    /**
+     * The generation of pack files in the temporary folder (from `os.tmpdir()`)
+     * sometimes fails in certain CI setups with the error message
+     * `unable to rename temporary pack file: Invalid cross-device link`.
+     * The reason why is unclear.
+     *
+     * A workaround is to attempt to generate the pack files in `process.cwd()`.
+     * While this works most of the times, it's not ideal since it affects the git status.
+     * This workaround is intended to be temporary.
+     *
+     * TODO: fix issue and remove workaround.
+     */
+    log.warn(`Failed generation of packfiles in tmpdir: ${err}`)
+    log.warn(`Generating them in ${process.cwd()} instead`)
+    const randomPrefix = String(Math.floor(Math.random() * 10000))
+    const cwdPath = path.join(process.cwd(), randomPrefix)
+    return generatePackfiles(cwdPath)
+  }
+}
+
+export const uploadPackfiles = async (log: Logger, request: RequestBuilder, repoURL: string, headCommit: string, packfilePaths: string[]) => {
+  // this loop makes sure requests are performed sequentially
+  for (const pack of packfilePaths) {
+    await uploadPackfile(log, request, repoURL, headCommit, pack)
+  }
+}
+
+export const uploadPackfile = async (log: Logger, request: RequestBuilder, repoURL: string, headCommit: string, packfilePath: string) => {
+  const pushedSha = JSON.stringify({
+    data: {
+      id: headCommit,
+      type: 'commit'
+    },
+    meta: {
+      repository_url: repoURL
+    }
+  })
+
+  const form = new FormData()
+
+  form.append('pushedSha', pushedSha, {contentType: 'application/json'})
+  const packFileContent = fs.readFileSync(packfilePath)
+  // The original filename includes a random prefix, so we remove it here
+  const [, filename] = path.basename(packfilePath).split('-')
+  form.append('packfile', packFileContent, {
+    filename,
+    contentType: 'application/octet-stream'
+  })
+
+  return await runRequest(log, 'pakfile', () => request({
+    url: '/api/v2/git/repository/packfile',
+    headers: {
+      ...form.getHeaders(),
+    },
+    timeout: API_TIMEOUT,
+    data: form,
+    method: 'POST',
+  }))
+}
+
+const runRequest = async <T>(log: Logger, reqName: string, request: () => Promise<AxiosResponse<T>>) => {
+  return await retryRequest(request, {
+    retries: 2,
+    onRetry: (e, attempt) => { // todo better type than any
+      let errorMessage = `${e}`
+      const maybeHttpError = e as any
+      if (maybeHttpError.response && maybeHttpError.response.statusText) {
+        errorMessage = `${maybeHttpError.message} (${maybeHttpError.response.statusText})`
+      }
+      log.warn(`[attempt ${attempt}] Retrying ${reqName} request: ${errorMessage}`)
+    },
+  })
+}
