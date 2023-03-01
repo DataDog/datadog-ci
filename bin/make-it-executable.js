45221f9e02997add608b49dd87ac786e49158fac
NoortsDD <124673795+NoortsDD@users.noreply.github.com>
2023-03-01 10:40:35 +0100

    Add support for single bin script path

--- bin/make-it-executable.js
+++ bin/make-it-executable.js
@@ -3,9 +3,19 @@ const path = require('path');
 const { promisify } = require('util');
 
 const ROOT = path.join(__dirname, '../');
-const scriptPaths = require(path.join(ROOT, './package.json')).bin;
+let scriptPaths = require(path.join(ROOT, './package.json')).bin;
 
 const INSERT = '#!/usr/bin/env node';
+
+/**
+ * The forEach loop below requires an object with script paths as properties to loop over. When the `bin` property in
+ * the package.json only has a single property, then the object is converted to a string upon a `yarn install` call
+ * (see: https://docs.npmjs.com/cli/v9/configuring-npm/package-json#bin). The string to object transformation here thus
+ * allows us to support 1+ properties whilst keeping the loop below.
+ */
+if (typeof scriptPaths === 'string') {
+  scriptPaths = { "datadog-ci": scriptPaths }
+}
 
 // Loop through all the bin we have in the package.json
 Object.values(scriptPaths).forEach(async script => {
