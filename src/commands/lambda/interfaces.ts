c257353e8f00c8df1c67b3717fc2b4fc3b9d4ede
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2023-03-23 21:15:39 -0400

    update `interfaces.ts`

--- src/commands/lambda/interfaces.ts
+++ src/commands/lambda/interfaces.ts
@@ -1,4 +1,16 @@
-import {CloudWatchLogs, Lambda} from 'aws-sdk'
+import {
+  CloudWatchLogsClient,
+  CreateLogGroupCommandInput,
+  DeleteSubscriptionFilterCommandInput,
+  PutSubscriptionFilterCommandInput,
+} from '@aws-sdk/client-cloudwatch-logs'
+import {
+  FunctionConfiguration as LFunctionConfiguration,
+  LambdaClient,
+  TagResourceCommandInput,
+  UpdateFunctionConfigurationCommandInput,
+  UntagResourceCommandInput,
+} from '@aws-sdk/client-lambda'
 
 /**
  * Configuration options provided by the user through
@@ -31,16 +43,16 @@ export interface LambdaConfigOptions {
  */
 export interface FunctionConfiguration {
   functionARN: string
-  lambdaConfig: Lambda.FunctionConfiguration
+  lambdaConfig: LFunctionConfiguration
   logGroupConfiguration?: LogGroupConfiguration
   tagConfiguration?: TagConfiguration
-  updateRequest?: Lambda.UpdateFunctionConfigurationRequest
+  updateFunctionConfigurationCommandInput?: UpdateFunctionConfigurationCommandInput
 }
 
 export interface InstrumentedConfigurationGroup {
-  cloudWatchLogs: CloudWatchLogs
+  cloudWatchLogsClient: CloudWatchLogsClient
   configs: FunctionConfiguration[]
-  lambda: Lambda
+  lambdaClient: LambdaClient
   region: string
 }
 
@@ -74,13 +86,13 @@ interface InstrumentationTags {
 }
 
 export interface LogGroupConfiguration {
-  createLogGroupRequest?: CloudWatchLogs.CreateLogGroupRequest
-  deleteSubscriptionFilterRequest?: CloudWatchLogs.DeleteSubscriptionFilterRequest
+  createLogGroupCommandInput?: CreateLogGroupCommandInput
+  deleteSubscriptionFilterCommandInput?: DeleteSubscriptionFilterCommandInput
   logGroupName: string
-  subscriptionFilterRequest?: CloudWatchLogs.PutSubscriptionFilterRequest
+  putSubscriptionFilterCommandInput?: PutSubscriptionFilterCommandInput
 }
 
 export interface TagConfiguration {
-  tagResourceRequest?: Lambda.TagResourceRequest
-  untagResourceRequest?: Lambda.UntagResourceRequest
+  tagResourceCommandInput?: TagResourceCommandInput
+  untagResourceCommandInput?: UntagResourceCommandInput
 }
