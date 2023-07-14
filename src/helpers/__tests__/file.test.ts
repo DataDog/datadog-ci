d204ca8f1d688ea086289acab18669aa080850fa
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-07-14 09:48:53 +0200

    bring back safe characters

--- src/helpers/__tests__/file.test.ts
+++ src/helpers/__tests__/file.test.ts
@@ -1,15 +1,21 @@
-import {replaceForwardSlashes} from '../file'
+import {replaceForwardSlashes, getSafeFileName} from '../file'
 
-describe('file util', () => {
-  describe('getSafeFileName', () => {
-    it('returns same file name if the file name is safe', () => {
-      const safeFileName = replaceForwardSlashes('myfilename')
-      expect(safeFileName).toBe('myfilename')
-    })
-    test('filters unsafe characters out', () => {
-      expect(replaceForwardSlashes('tests/reports/junit/integration.xml')).toEqual(
-        'tests\\reports\\junit\\integration.xml'
-      )
-    })
+describe('replaceForwardSlashes', () => {
+  it('returns same file name if the file name does not include forward slashes', () => {
+    expect(replaceForwardSlashes('myfilename')).toBe('myfilename')
+  })
+  test('replaces forward slashes', () => {
+    expect(replaceForwardSlashes('tests/reports/junit/integration.xml')).toEqual(
+      'tests\\reports\\junit\\integration.xml'
+    )
+  })
+})
+
+describe('getSafeFileName', () => {
+  it('returns same file name if the file name is safe', () => {
+    expect(replaceForwardSlashes('myfilename')).toBe('myfilename')
+  })
+  test('filters unsafe characters out', () => {
+    expect(getSafeFileName('http://gitlab.com/-/pipelines/12345')).toEqual('http___gitlab_com___pipelines_12345')
   })
 })
