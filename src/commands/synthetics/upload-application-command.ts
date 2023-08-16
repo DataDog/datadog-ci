2da8e019f864fbd52810db29f3ce8619a05bc16c
Corentin Girard <corentin.girard@datadoghq.com>
2023-08-16 19:23:10 +0200

    [synthetics] Add details and examples in `--help` (#1032)
    * Add `--version` and `--help` root arguments
    
    * Add a usage with `category` and `description` for all commands
    
    * Add unit tests for `src/cli.ts`
    
    * [run-tests] Add details, examples and argument descriptions
    
    * [upload-application] Add details, examples and argument descriptions
    
    * Improve "Run tests" section of the README

--- src/commands/synthetics/upload-application-command.ts
+++ src/commands/synthetics/upload-application-command.ts
@@ -1,5 +1,6 @@
 import {Command, Option} from 'clipanion'
 import deepExtend from 'deep-extend'
+import terminalLink from 'terminal-link'
 
 import {LogLevel, Logger} from '../../helpers/logger'
 import {removeUndefinedValues, resolveConfigFromFile} from '../../helpers/utils'
@@ -21,22 +22,43 @@ export const DEFAULT_UPLOAD_COMMAND_CONFIG: UploadApplicationCommandConfig = {
   latest: false,
 }
 
+const configurationLink = 'https://docs.datadoghq.com/continuous_testing/cicd_integrations/configuration'
+
+const $1 = (text: string) => terminalLink(text, `${configurationLink}#global-configuration-file-options`)
+
 export class UploadApplicationCommand extends Command {
   public static paths = [['synthetics', 'upload-application']]
 
   public static usage = Command.Usage({
     category: 'Synthetics',
     description: 'Upload a new version to an existing application in Datadog.',
+    details: `
+      This command will upload a \`.apk\` or \`.ipa\` file as a new version for a given application, which already exists in Datadog.\n
+      https://docs.datadoghq.com/mobile_app_testing/mobile_app_tests
+    `,
+    examples: [
+      [
+        'Upload version `example 1.0` and mark it as latest',
+        "datadog-ci synthetics upload-application --mobileApplicationId '123-123-123' --mobileApplicationVersionFilePath example/test.apk --versionName 'example 1.0' --latest",
+      ],
+    ],
   })
 
-  private apiKey = Option.String('--apiKey')
-  private appKey = Option.String('--appKey')
-  private configPath = Option.String('--config')
-  private datadogSite = Option.String('--datadogSite')
-  private mobileApplicationVersionFilePath = Option.String('--mobileApp,--mobileApplicationVersionFilePath')
-  private mobileApplicationId = Option.String('--mobileApplicationId')
-  private versionName = Option.String('--versionName')
-  private latest = Option.Boolean('--latest')
+  private apiKey = Option.String('--apiKey', {description: 'The API key used to query the Datadog API.'})
+  private appKey = Option.String('--appKey', {description: 'The application key used to query the Datadog API.'})
+  private configPath = Option.String('--config', {description: `Pass a path to a ${$1('global configuration file')}.`})
+  private datadogSite = Option.String('--datadogSite', {description: 'The Datadog instance to which request is sent.'})
+  private mobileApplicationVersionFilePath = Option.String('--mobileApp,--mobileApplicationVersionFilePath', {
+    description: 'Override the application version for all Synthetic mobile application tests.',
+  })
+  private mobileApplicationId = Option.String('--mobileApplicationId', {
+    description: 'ID of the application you want to upload the new version to.',
+  })
+  private versionName = Option.String('--versionName', {description: 'Name of the new version. It has to be unique.'})
+  private latest = Option.Boolean('--latest', {
+    description:
+      'Marks the application as `latest`. Any tests that run on the latest version will use this version on their next run.',
+  })
 
   private config: UploadApplicationCommandConfig = JSON.parse(JSON.stringify(DEFAULT_UPLOAD_COMMAND_CONFIG)) // Deep copy to avoid mutation during unit tests
 
