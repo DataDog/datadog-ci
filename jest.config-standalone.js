5669c997074e6b004a6f88a4cfe4d4d628c68955
corentin <corentin.girard@datadoghq.com>
2023-10-31 10:16:55 +0100

    Fix jest config

--- jest.config-standalone.js
+++ jest.config-standalone.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
   roots: ['standalone-e2e'],
 }
