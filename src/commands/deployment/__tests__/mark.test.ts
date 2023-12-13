40969a74ef661f36a70e7f31b6f4ae6edd664dd4
Andrea Moscatelli <andrea.moscatelli@datadoghq.com>
2023-12-13 16:40:00 +0100

    Fixing tests

--- src/commands/deployment/__tests__/mark.test.ts
+++ src/commands/deployment/__tests__/mark.test.ts
@@ -7,26 +7,26 @@ describe('mark', () => {
     test('should add is rollback if present', () => {
       const command = createCommand(DeploymentMarkCommand)
       command['isRollback'] = true
-      const expectedTags = ['datadog_cd_visibility.enabled:true', 'datadog_cd_visibility.is_rollback:true']
+      const expectedTags = ['datadog_cd_visibility.is_deployment:true', 'datadog_cd_visibility.is_rollback:true']
       expect(command.createDeploymentTags()).toEqual(expectedTags)
     })
     test('should add env if present', () => {
       const command = createCommand(DeploymentMarkCommand)
       command['env'] = 'test'
-      const expectedTags = ['datadog_cd_visibility.enabled:true', 'datadog_cd_visibility.env:test']
+      const expectedTags = ['datadog_cd_visibility.is_deployment:true', 'datadog_cd_visibility.env:test']
       expect(command.createDeploymentTags()).toEqual(expectedTags)
     })
     test('should add revision if present', () => {
       const command = createCommand(DeploymentMarkCommand)
       command['revision'] = '1.0.0'
-      const expectedTags = ['datadog_cd_visibility.enabled:true', 'datadog_cd_visibility.revision:1.0.0']
+      const expectedTags = ['datadog_cd_visibility.is_deployment:true', 'datadog_cd_visibility.revision:1.0.0']
       expect(command.createDeploymentTags()).toEqual(expectedTags)
     })
     test('should add custom tags if present', () => {
       const command = createCommand(DeploymentMarkCommand)
       command['tags'] = ['team:backend', 'image:my_image']
       const expectedTags = [
-        'datadog_cd_visibility.enabled:true',
+        'datadog_cd_visibility.is_deployment:true',
         'datadog_cd_visibility.custom_tags:team:backend,image:my_image',
       ]
       expect(command.createDeploymentTags()).toEqual(expectedTags)
