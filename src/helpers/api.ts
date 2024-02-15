327b5d570ea40d0a9da777defd7ee5465c1422a9
TophrC-dd <topher.campbell@datadoghq.com>
2024-02-15 12:14:55 -0500

    remove whitespacing

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,7 +2,6 @@ import {DATADOG_SITE_US1} from '../constants'
 
 export const datadogSite =  process.env.DATADOG_SITE || process.env.DD_SITE || DATADOG_SITE_US1
 
-
 /**
  * Get the base intake URL for a service. If the `DD_SITE` or `DATADOG_SITE` environment
  * variables are not defined, use the default site (US1).
