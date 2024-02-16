52a49337a8acca2b510393442bfd4bc7ab90555c
TophrC-dd <topher.campbell@datadoghq.com>
2024-02-16 11:59:45 -0500

    Fixing comment to match datadog language

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,9 +1,9 @@
 import {DATADOG_SITE_US1} from '../constants'
 
 /**
- *  Get the site domain by reading DATADOG_SITE or DD_SITE
- *  Otherwise we go with US1 as default
- */
+* Get Datadog site's domain. If the `DD_SITE` or `DATADOG_SITE` environment
+* variables are not defined, use the default site (US1). 
+*/
 export const getDatadogSite = () => {
   return process.env.DATADOG_SITE || process.env.DD_SITE || DATADOG_SITE_US1
 }
