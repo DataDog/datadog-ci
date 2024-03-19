bbd4b2f24c3ffc1a86c49abcf110adbf28edeac4
Omer Cohen <git@omer.io>
2024-03-19 14:30:28 +0200

    fix(sbom): detect npm from purl

--- src/commands/sbom/language.ts
+++ src/commands/sbom/language.ts
@@ -6,7 +6,7 @@ export const getLanguageFromComponent = (component: any): DependencyLanguage | u
   const componentName = component['name']
 
   if (component['bom-ref']) {
-    if (component['bom-ref'].includes('pkg:npm')) {
+    if (component['bom-ref'].includes('pkg:npm') || component['purl'].includes('pkg:npm')) {
       return DependencyLanguage.NPM
     }
     if (component['purl'].includes('pkg:composer')) {
