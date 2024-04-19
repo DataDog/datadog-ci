53edb3c42a62637c8bba0226dd6f90e18fa313bd
Juan Antonio Fernández de Alba <juan.fernandezdealba@datadoghq.com>
2024-04-19 17:26:56 +0200

    [git upload] Improvements to git upload and change junit git upload default  (#1287)

--- src/commands/junit/upload.ts
+++ src/commands/junit/upload.ts
@@ -128,7 +128,7 @@ export class UploadJUnitXMLCommand extends Command {
   private reportMeasures = Option.Array('--report-measures')
   private rawXPathTags = Option.Array('--xpath-tag')
   private gitRepositoryURL = Option.String('--git-repository-url')
-  private skipGitMetadataUpload = Option.String('--skip-git-metadata-upload', 'true', {
+  private skipGitMetadataUpload = Option.String('--skip-git-metadata-upload', 'false', {
     validator: t.isBoolean(),
     tolerateBoolean: true,
   })
