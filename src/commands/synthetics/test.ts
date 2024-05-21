aab87c52360083baa65cd6daf8e4db52d3d6567e
Teodor Todorov <teodor.todorov@datadoghq.com>
2024-05-21 17:24:15 +0200

    fix linting again

--- src/commands/synthetics/test.ts
+++ src/commands/synthetics/test.ts
@@ -17,7 +17,9 @@ export const getTestConfigs = async (
   suites.push(...suitesFromFiles)
 
   // TODO SYNTH-12989: Clean up deprecated `config` in favor of `testOverrides`
-  const isUsingConfig = suites.some((suite) => suite.content.tests.some((test) => Object.keys(test.config ?? {}).length > 0))
+  const isUsingConfig = suites.some((suite) =>
+    suite.content.tests.some((test) => Object.keys(test.config ?? {}).length > 0)
+  )
   if (isUsingConfig) {
     reporter?.error(
       "The 'config' property is deprecated. Please use 'testOverrides' instead.\nIf both 'config' and 'testOverrides' properties exist, 'testOverrides' is used!\n"
