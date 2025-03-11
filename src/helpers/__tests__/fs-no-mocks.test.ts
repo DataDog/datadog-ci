a1325cc4a01095d3fdd12bbf7a77437fb3ef25ad
corentin <corentin.girard@datadoghq.com>
2025-03-11 16:29:23 +0100

    Fix test

--- src/helpers/__tests__/fs-no-mocks.test.ts
+++ src/helpers/__tests__/fs-no-mocks.test.ts
@@ -3,28 +3,28 @@ import {globAsync, globSync} from '../fs'
 describe('fs', () => {
   describe('globSync', () => {
     it('uses default options', () => {
-      expect(globSync('src/helpers/__tests__/*.platform-specific.test.ts')).toStrictEqual([
-        'src/helpers/__tests__/fs.platform-specific.test.ts',
+      expect(globSync('src/helpers/__tests__/*-no-mocks.test.ts')).toStrictEqual([
+        'src/helpers/__tests__/fs-no-mocks.test.ts',
       ])
     })
 
     it('works with dotRelative option', () => {
-      expect(globSync('src/helpers/__tests__/*.platform-specific.test.ts', {dotRelative: true})).toStrictEqual([
-        './src/helpers/__tests__/fs.platform-specific.test.ts',
+      expect(globSync('src/helpers/__tests__/*-no-mocks.test.ts', {dotRelative: true})).toStrictEqual([
+        './src/helpers/__tests__/fs-no-mocks.test.ts',
       ])
     })
   })
 
   describe('globAsync', () => {
     it('uses default options', async () => {
-      expect(await globAsync('src/helpers/__tests__/*.platform-specific.test.ts')).toStrictEqual([
-        'src/helpers/__tests__/fs.platform-specific.test.ts',
+      expect(await globAsync('src/helpers/__tests__/*-no-mocks.test.ts')).toStrictEqual([
+        'src/helpers/__tests__/fs-no-mocks.test.ts',
       ])
     })
 
     it('works with dotRelative option', async () => {
-      expect(await globAsync('src/helpers/__tests__/*.platform-specific.test.ts', {dotRelative: true})).toStrictEqual([
-        './src/helpers/__tests__/fs.platform-specific.test.ts',
+      expect(await globAsync('src/helpers/__tests__/*-no-mocks.test.ts', {dotRelative: true})).toStrictEqual([
+        './src/helpers/__tests__/fs-no-mocks.test.ts',
       ])
     })
   })
