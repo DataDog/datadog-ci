e8a5d6b8b0b374b2aefeb902ae6d089d3f94168c
Teodor Todorov <teodor.todorov@datadoghq.com>
2025-03-11 13:18:53 +0100

    try again

--- src/commands/synthetics/__tests__/utils/public.test.ts
+++ src/commands/synthetics/__tests__/utils/public.test.ts
@@ -44,7 +44,7 @@ import process from 'process'
 
 import type * as path from 'path'
 
-import * as glob from 'glob'
+import glob from 'glob'
 
 import {getAxiosError} from '../../../../helpers/__tests__/fixtures'
 
@@ -87,7 +87,7 @@ describe('utils', () => {
       file2: '{"tests":"file2"}',
     }
 
-    jest.spyOn(glob, 'sync').mockReturnValueOnce(FILES)
+    jest.spyOn(glob, 'sync').mockReturnValue(FILES)
     ;(fs.readFile as any).mockImplementation((path: 'file1' | 'file2', opts: any, callback: any) =>
       callback(undefined, FILES_CONTENT[path])
     )
