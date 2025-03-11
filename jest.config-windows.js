502984505e56c49441dbbc17a2bf359d4ad164c0
corentin <corentin.girard@datadoghq.com>
2025-03-11 19:25:54 +0100

    Add `yarn test:windows`

---
+++ jest.config-windows.js
@@ -0,0 +1,19 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['react-native'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+  roots: ['src'],
+}
