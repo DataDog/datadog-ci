01f7cb4bc23581ed2544559bc60a87396cbcea0d
corentin <corentin.girard@datadoghq.com>
2025-04-02 16:48:56 +0200

    Revert `.win.snap`

--- jest.config-windows.js
+++ jest.config-windows.js
@@ -5,7 +5,6 @@
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
-  snapshotResolver: './etc/snapshot-resolver.windows.js',
   testMatch: ['**/__tests__/**/*.test.ts'],
   testPathIgnorePatterns: ['react-native', 'dsyms'],
   transform: {
