778a24bf4392985a4806776f9da48c72dc3169f9
corentin <corentin.girard@datadoghq.com>
2025-04-17 18:30:08 +0200

    Fix leftover

--- src/commands/sourcemaps/upload.ts
+++ src/commands/sourcemaps/upload.ts
@@ -182,7 +182,7 @@ export class UploadCommand extends Command {
   // Looks for the sourcemaps and minified files on disk and returns
   // the associated payloads.
   private getMatchingSourcemapFiles = async (): Promise<Sourcemap[]> => {
-    const sourcemapFiles = globSync(buildPath(this.basePath, '**/*js.map'), {posix: true})
+    const sourcemapFiles = globSync(buildPath(this.basePath, '**/*js.map'))
 
     return Promise.all(
       sourcemapFiles.map(async (sourcemapPath) => {
