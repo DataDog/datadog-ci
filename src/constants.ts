92a3c748fef2e164738b49e4acbbef24527377ee
Adrien Letournel <adrien.letournel@datadoghq.com>
2025-06-02 11:22:13 +0200

    feat(SYNTH-20126): add AP2 in datadog-ci

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const DATADOG_SITE_EU1 = 'datadoghq.eu'
 export const DATADOG_SITE_US3 = 'us3.datadoghq.com'
 export const DATADOG_SITE_US5 = 'us5.datadoghq.com'
 export const DATADOG_SITE_AP1 = 'ap1.datadoghq.com'
+export const DATADOG_SITE_AP2 = 'ap2.datadoghq.com'
 export const DATADOG_SITE_GOV = 'ddog-gov.com'
 
 export const DATADOG_SITES: string[] = [
@@ -11,6 +12,7 @@ export const DATADOG_SITES: string[] = [
   DATADOG_SITE_US3,
   DATADOG_SITE_US5,
   DATADOG_SITE_AP1,
+  DATADOG_SITE_AP2,
   DATADOG_SITE_GOV,
 ]
 
