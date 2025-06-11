c181177e395e164af5df44524e97d793fde8c23f
Ava Silver <ava.silver@datadoghq.com>
2025-06-11 15:00:13 -0400

    [SVLS-6923] fix sidecar application settings

--- src/commands/aas/instrument.ts
+++ src/commands/aas/instrument.ts
@@ -1,8 +1,8 @@
-import {StringDictionary, WebSiteManagementClient} from '@azure/arm-appservice'
+import {SiteContainer, StringDictionary, WebSiteManagementClient} from '@azure/arm-appservice'
 import {DefaultAzureCredential} from '@azure/identity'
 import chalk from 'chalk'
-import {Command} from 'clipanion'
-import equal from 'fast-deep-equal'
+import {Command, Option} from 'clipanion'
+import equal from 'fast-deep-equal/es6'
 
 import {renderError, renderSoftWarning} from '../../helpers/renderer'
 
@@ -98,14 +98,15 @@ https://docs.datadoghq.com/serverless/azure_app_services/azure_app_services_wind
     const siteContainers = await collectAsyncIterator(client.webApps.listSiteContainers(resourceGroup, aasName))
     const sidecarContainer = siteContainers.find((c) => c.name === SIDECAR_CONTAINER_NAME)
     const envVars = this.getEnvVars(config)
+    // We need to ensure that the sidecar container is configured correctly, which means checking the image, target port,
+    // and environment variables. The environment variables are must have the matching names and values as the
+    // keys in the App Settings, since the value indicates the name of the environment variable.
     if (
       sidecarContainer === undefined ||
       sidecarContainer.image !== SIDECAR_IMAGE ||
       sidecarContainer.targetPort !== SIDECAR_PORT ||
-      !equal(
-        Object.fromEntries(sidecarContainer.environmentVariables?.map(({name, value}) => [name, value]) ?? []),
-        envVars
-      )
+      !sidecarContainer.environmentVariables?.every(({name, value}) => name === value) ||
+      equal(new Set(sidecarContainer.environmentVariables.map(({name}) => name)), new Set(Object.keys(envVars)))
     ) {
       this.context.stdout.write(
         `${this.dryRunPrefix}${sidecarContainer === undefined ? 'Creating' : 'Updating'} sidecar container ${chalk.bold(
@@ -117,7 +118,7 @@ https://docs.datadoghq.com/serverless/azure_app_services/azure_app_services_wind
           image: SIDECAR_IMAGE,
           targetPort: SIDECAR_PORT,
           isMain: false,
-          environmentVariables: Object.entries(envVars).map(([name, value]) => ({name, value})),
+          environmentVariables: Object.keys(envVars).map((name) => ({name, value: name})),
         })
       }
     } else {
