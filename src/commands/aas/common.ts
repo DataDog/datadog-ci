573b534a21b9d6c5da9e037adb1687a46fccc9ac
Ava Silver <ava.silver@datadoghq.com>
2025-06-16 14:42:15 -0400

    [SVLS-7005] pr feedback

--- src/commands/aas/common.ts
+++ src/commands/aas/common.ts
@@ -15,6 +15,18 @@ export const SIDECAR_CONTAINER_NAME = 'datadog-sidecar'
 export const SIDECAR_IMAGE = 'index.docker.io/datadog/serverless-init:latest'
 export const SIDECAR_PORT = '8126'
 
+
+// Path to tracing libraries, copied within the Docker file
+const DD_DOTNET_TRACER_HOME = '/home/site/wwwroot/datadog'
+// Where tracer logs are stored
+const DD_TRACE_LOG_DIRECTORY = '/home/LogFiles/dotnet'
+// Instructs the .NET CLR that profiling should be enabled
+const CORECLR_ENABLE_PROFILING = '1'
+// Profiler GUID
+const CORECLR_PROFILER = '{846F5F1C-F9AE-4B07-969E-05C26BC060D8}'
+// The profiler binary that the .NET CLR loads into memory, which contains the GUID
+const CORECLR_PROFILER_PATH = '/home/site/wwwroot/datadog/linux-musl-x64/Datadog.Trace.ClrProfiler.Native.so'
+
 export abstract class AasCommand extends Command {
   public dryRun = Option.Boolean('-d,--dry-run', false, {
     description: 'Run the command in dry-run mode, without making any changes',
@@ -46,7 +58,7 @@ export abstract class AasCommand extends Command {
   })
 
   private isDotnet = Option.Boolean('--dotnet', false, {
-    description: 'Add in required .NET-specific configuration options, is automatically inferred for code runtimes',
+    description: 'Add in required .NET-specific configuration options, is automatically inferred for code runtimes. This should be specified if you are using a containerized .NET app.',
   })
 
   private fips = Option.Boolean('--fips', false)
@@ -122,11 +134,11 @@ export const getEnvVars = (config: AasConfigOptions): Record<string, string> => 
     envVars = {
       ...envVars,
 
-      DD_DOTNET_TRACER_HOME: '/home/site/wwwroot/datadog',
-      DD_TRACE_LOG_DIRECTORY: '/home/LogFiles/dotnet',
-      CORECLR_ENABLE_PROFILING: '1',
-      CORECLR_PROFILER: '{846F5F1C-F9AE-4B07-969E-05C26BC060D8}',
-      CORECLR_PROFILER_PATH: '/home/site/wwwroot/datadog/linux-musl-x64/Datadog.Trace.ClrProfiler.Native.so',
+      DD_DOTNET_TRACER_HOME,
+      DD_TRACE_LOG_DIRECTORY,
+      CORECLR_ENABLE_PROFILING,
+      CORECLR_PROFILER,
+      CORECLR_PROFILER_PATH,
     }
   }
 
