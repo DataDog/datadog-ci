8a2d31157b0e74b2906de9675ea3d743cb2f4338
Ava Silver <ava.silver@datadoghq.com>
2025-06-27 18:36:05 -0400

    [SVLS-3622] format

--- src/helpers/version.ts
+++ src/helpers/version.ts
@@ -8,6 +8,7 @@ const REGISTRY_URL = 'https://registry.npmjs.org/@datadog/datadog-ci'
 
 export const getLatestVersion = async (): Promise<string> => {
   const response = await fetch(REGISTRY_URL)
-  const data = await response.json() as { 'dist-tags': { latest: string } }
+  const data = (await response.json()) as {'dist-tags': {latest: string}}
+
   return data['dist-tags'].latest
 }
