55d8f9f52dc06191ef1ff0a8c22caaf69d810e9a
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-07-01 12:57:13 -0400

    fix comment for better IDE docs

--- src/constants.ts
+++ src/constants.ts
@@ -45,10 +45,12 @@ export const DD_LLMOBS_ENABLED_ENV_VAR = 'DD_LLMOBS_ENABLED'
 export const DD_LLMOBS_ML_APP_ENV_VAR = 'DD_LLMOBS_ML_APP'
 export const DD_LLMOBS_AGENTLESS_ENABLED_ENV_VAR = 'DD_LLMOBS_AGENTLESS_ENABLED'
 export const DD_TAGS_ENV_VAR = 'DD_TAGS'
-// DD_TAGS Regular Expression
-// This RegExp ensures that the --extra-tags string
-// matches a list of <key>:<value> separated by commas
-// such as: layer:api,team:intake
+/*
+ * DD_TAGS Regular Expression
+ * This RegExp ensures that the --extra-tags string
+ * matches a list of <key>:<value> separated by commas
+ * such as layer:api,team:intake
+ */
 export const EXTRA_TAGS_REG_EXP = /^(([a-zA-Z]+)[\w\-/\.]*:[^,]+)+((\,)([a-zA-Z]+)[\w\-/\.]*:[^,]+)*$/g
 
 // Flare constants
