6195d67451fe982d5bc9512849bbb9e6a54a0ead
corentin <corentin.girard@datadoghq.com>
2025-07-07 15:01:05 +0200

    fix conflict

--- src/commands/junit/utils.ts
+++ src/commands/junit/utils.ts
@@ -2,13 +2,6 @@ import {getBaseUrl} from '../../helpers/app'
 import {SpanTags} from '../../helpers/interfaces'
 import {CI_JOB_URL, CI_PIPELINE_URL, GIT_BRANCH, GIT_REPOSITORY_URL, GIT_SHA} from '../../helpers/tags'
 
-export const getBaseUrl = () => {
-  const site = process.env.DATADOG_SITE || process.env.DD_SITE || 'datadoghq.com'
-  const subdomain = process.env.DD_SUBDOMAIN || ''
-
-  return getCommonAppBaseURL(site, subdomain)
-}
-
 export const getTestRunsUrlPath = (spanTags: SpanTags, queryPrefix = ''): string => {
   if (!spanTags[CI_PIPELINE_URL] && !spanTags[CI_JOB_URL]) {
     return ''
