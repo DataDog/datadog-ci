a7775505fe833ca4aeaaebc29cd7b4ed0a8076ef
corentin <corentin.girard@datadoghq.com>
2025-09-02 11:08:35 +0200

    Make `yarn build` work

--- packages/datadog-ci/src/index.ts
+++ packages/datadog-ci/src/index.ts
@@ -1,4 +1,4 @@
 export * as gitMetadata from './commands/git-metadata'
-export * as synthetics from './commands/synthetics'
+export * as synthetics from '@datadog/datadog-ci-plugin-synthetics'
 export * as utils from '@datadog/datadog-ci-base/helpers/utils'
 export {cliVersion as version} from './version'
