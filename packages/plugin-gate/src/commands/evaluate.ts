ccb032377717137c88493c752f1dbf51b9326dad
Vincent Barbaresi <vincent.barbaresi@datadoghq.com>
2025-09-09 15:32:12 +0200

    fix the logic in timeoutInSeconds

--- packages/plugin-gate/src/commands/evaluate.ts
+++ packages/plugin-gate/src/commands/evaluate.ts
@@ -149,7 +149,7 @@ export class PluginCommand extends GateEvaluateCommand {
     bail?: (e: Error) => void
   ): Promise<AxiosResponse<EvaluationResponsePayload>> {
     const timePassed = new Date().getTime() - evaluateRequest.startTimeMs
-    const timeoutInSeconds = parseInt(String(this.timeoutInSeconds), 10) || this.defaultTimeout
+    const timeoutInSeconds = this.timeoutInSeconds !== undefined ? parseInt(String(this.timeoutInSeconds), 10) : this.defaultTimeout
     const remainingWait = timeoutInSeconds * 1000 - timePassed
 
     return new Promise((resolve, reject) => {
