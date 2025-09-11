25e0fe4c9dcebfe67b21b7822acf130c13133455
Ava Silver <ava.silver@datadoghq.com>
2025-09-11 15:41:19 -0400

    [PACKAGES-MIGRATION] pr feedback

--- packages/base/src/helpers/git/source-code-integration.ts
+++ packages/base/src/helpers/git/source-code-integration.ts
@@ -1,59 +1,11 @@
 import {BaseContext, Cli} from 'clipanion'
 
-import {getCommitInfo, newSimpleGit} from '../commands/git-metadata/git'
-import {UploadCommand} from '../commands/git-metadata/upload'
+import {getCommitInfo, newSimpleGit} from '../../commands/git-metadata/git'
+import {UploadCommand} from '../../commands/git-metadata/upload'
 
-import {renderSoftWarning} from './renderer'
-import {filterAndFormatGithubRemote} from './utils'
+import {renderSoftWarning} from '../renderer'
+import {filterAndFormatGithubRemote} from '../utils'
 
-const getGitData = async () => {
-  let currentStatus
-
-  try {
-    currentStatus = await getCurrentGitStatus()
-  } catch (err) {
-    throw Error("Couldn't get local git status")
-  }
-
-  if (!currentStatus.isClean) {
-    throw Error('Local git repository is dirty')
-  }
-
-  if (currentStatus.ahead > 0) {
-    throw Error('Local changes have not been pushed remotely. Aborting git data tagging.')
-  }
-
-  const gitRemote = filterAndFormatGithubRemote(currentStatus.remote)
-
-  return {commitSha: currentStatus.hash, gitRemote}
-}
-
-export const getCurrentGitStatus = async () => {
-  const simpleGit = await newSimpleGit()
-  const gitCommitInfo = await getCommitInfo(simpleGit)
-  if (gitCommitInfo === undefined) {
-    throw new Error('Git commit info is not defined')
-  }
-  const status = await simpleGit.status()
-
-  return {
-    isClean: status.isClean(),
-    ahead: status.ahead,
-    files: status.files,
-    hash: gitCommitInfo?.hash,
-    remote: gitCommitInfo?.remote,
-  }
-}
-
-export const uploadGitData = async (context: BaseContext) => {
-  const cli = new Cli()
-  cli.register(UploadCommand)
-  if ((await cli.run(['git-metadata', 'upload'], context)) !== 0) {
-    throw Error("Couldn't upload git metadata")
-  }
-
-  return
-}
 
 export const handleSourceCodeIntegration = async (
   context: BaseContext,
@@ -76,3 +28,57 @@ export const handleSourceCodeIntegration = async (
 
   return extraTags
 }
+
+
+// Only exported to be mocked in unit tests
+const getGitData = async () => {
+  let currentStatus
+
+  try {
+    currentStatus = await getCurrentGitStatus()
+  } catch (err) {
+    throw Error("Couldn't get local git status")
+  }
+
+  if (!currentStatus.isClean) {
+    throw Error('Local git repository is dirty')
+  }
+
+  if (currentStatus.ahead > 0) {
+    throw Error('Local changes have not been pushed remotely. Aborting git data tagging.')
+  }
+
+  const gitRemote = filterAndFormatGithubRemote(currentStatus.remote)
+
+  return {commitSha: currentStatus.hash, gitRemote}
+}
+
+// Only exported to be mocked in unit tests
+export const getCurrentGitStatus = async () => {
+  const simpleGit = await newSimpleGit()
+  const gitCommitInfo = await getCommitInfo(simpleGit)
+  if (gitCommitInfo === undefined) {
+    throw new Error('Git commit info is not defined')
+  }
+  const status = await simpleGit.status()
+
+  return {
+    isClean: status.isClean(),
+    ahead: status.ahead,
+    files: status.files,
+    hash: gitCommitInfo?.hash,
+    remote: gitCommitInfo?.remote,
+  }
+}
+
+// Only exported to be mocked in unit tests
+export const uploadGitData = async (context: BaseContext) => {
+  const cli = new Cli()
+  cli.register(UploadCommand)
+  if ((await cli.run(['git-metadata', 'upload'], context)) !== 0) {
+    throw Error("Couldn't upload git metadata")
+  }
+
+  return
+}
+
