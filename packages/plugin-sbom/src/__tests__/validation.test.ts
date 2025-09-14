f7def28ba3f9767cd13cb679d3de3f806149f9a2
Daniel Strong <daniel.strong@datadoghq.com>
2025-09-14 15:23:11 -0400

    Fix prettier issues

--- packages/plugin-sbom/src/__tests__/validation.test.ts
+++ packages/plugin-sbom/src/__tests__/validation.test.ts
@@ -17,12 +17,8 @@ const validator = getValidator()
 
 describe('validation of sbom file', () => {
   test('should succeed when called on a valid CycloneDX 1.4 SBOM file', () => {
-    expect(
-      validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.4.ok.json', validator, false)
-    ).toBeTruthy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.5.ok.json', false)
-    ).toBeTruthy()
+    expect(validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.4.ok.json', validator, false)).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.5.ok.json', false)).toBeTruthy()
   })
 
   test('should succeed when called on a valid CycloneDX 1.6 SBOM file', () => {
@@ -32,21 +28,15 @@ describe('validation of sbom file', () => {
   })
 
   test('should succeed when called on a valid CycloneDX 1.5 SBOM file', () => {
-    expect(
-      validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.5.ok.json', validator, false)
-    ).toBeTruthy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.5.ok.json', false)
-    ).toBeTruthy()
+    expect(validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.5.ok.json', validator, false)).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.5.ok.json', false)).toBeTruthy()
   })
 
   test('should fail on files that do not exist', () => {
     expect(
       validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.4.do.not.exists', validator, false)
     ).toBeFalsy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.4.do.not.exists', false)
-    ).toBeFalsy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.4.do.not.exists', false)).toBeFalsy()
   })
 
   test('should fail on files that do not satisfy the schema', () => {
@@ -54,19 +44,13 @@ describe('validation of sbom file', () => {
     expect(
       validateSbomFileAgainstSchema('./src/__tests__/fixtures/sbom.1.4.invalid.json', validator, false)
     ).toBeFalsy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.4.invalid.json', false)
-    ).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom.1.4.invalid.json', false)).toBeTruthy()
   })
 
   test('should validate SBOM file from trivy 4.9', () => {
     // type and name of the "component" have been removed from the valid file.
-    expect(
-      validateSbomFileAgainstSchema('./src/__tests__/fixtures/trivy-4.9.json', validator, true)
-    ).toBeTruthy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/trivy-4.9.json', true)
-    ).toBeTruthy()
+    expect(validateSbomFileAgainstSchema('./src/__tests__/fixtures/trivy-4.9.json', validator, true)).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/trivy-4.9.json', true)).toBeTruthy()
   })
 
   test('should validate SBOM file osv scanner - version 1.5', () => {
@@ -74,21 +58,15 @@ describe('validation of sbom file', () => {
     expect(
       validateSbomFileAgainstSchema('./src/__tests__/fixtures/osv-scanner-files.json', validator, true)
     ).toBeTruthy()
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/osv-scanner-files.json', true)
-    ).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/osv-scanner-files.json', true)).toBeTruthy()
   })
 
   test('SBOM with no components is valid', () => {
-    expect(
-      validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom-no-components.json', true)
-    ).toBeTruthy()
+    expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/sbom-no-components.json', true)).toBeTruthy()
   })
 
   test('does not validate random data', () => {
-    expect(
-      validateSbomFileAgainstSchema('./src/__tests__/fixtures/random-data', validator, true)
-    ).toBeFalsy()
+    expect(validateSbomFileAgainstSchema('./src/__tests__/fixtures/random-data', validator, true)).toBeFalsy()
     expect(validateFileAgainstToolRequirements('./src/__tests__/fixtures/random-data', true)).toBeFalsy()
   })
 
