735c833a6fb5726ea9ab622aed534acb4e2cfb3a
corentin <corentin.girard@datadoghq.com>
2025-09-29 16:30:14 +0200

    Create script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -408,7 +408,7 @@ export default defineConfig(
     },
   },
   {
-    files: ['jest.config*.mjs', 'jest.setup.ts', 'packages/datadog-ci/scripts/esbuild.mjs'],
+    files: ['jest.config*.mjs', 'jest.setup.ts', 'bin/*.ts', 'packages/datadog-ci/scripts/esbuild.mjs'],
     extends: [tseslint.configs.disableTypeChecked],
   },
   {
