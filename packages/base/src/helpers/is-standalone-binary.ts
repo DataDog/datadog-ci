92edeee3a777b93405f3825a497fa3c3597bd238
corentin <corentin.girard@datadoghq.com>
2025-09-30 19:21:20 +0200

    Fix `@types/node` warning

--- packages/base/src/helpers/is-standalone-binary.ts
+++ packages/base/src/helpers/is-standalone-binary.ts
@@ -1,6 +1,5 @@
 export const isStandaloneBinary = async () => {
   try {
-    // @ts-expect-error The types for `node:sea` are defined in `@types/node@^20.19.13` but moving from `^18.19.76` to `^20.19.13` makes the build fail. We'll ignore the error for now.
     const {isSea} = await import('node:sea')
 
     return isSea()
