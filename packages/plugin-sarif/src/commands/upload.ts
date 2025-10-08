a5c7c8a997279c83e7e519a04502a12ae0d71de5
Cole Maring <65455664+colemaring@users.noreply.github.com>
2025-10-08 09:39:49 -0400

    Update packages/plugin-sarif/src/commands/upload.ts
    Co-authored-by: Corentin Girard <corentin.girard@datadoghq.com>

--- packages/plugin-sarif/src/commands/upload.ts
+++ packages/plugin-sarif/src/commands/upload.ts
@@ -51,7 +51,7 @@ export class PluginCommand extends SarifUploadCommand {
     if (githubEvent === 'pull_request') {
       // https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-commands#example-setting-an-error-message
       this.context.stdout.write(
-        '::error title=Unsupported Trigger::The pull_request trigger is not supported by Datadog Code Security and will cause issues with the product. ' +
+        '::error title=Unsupported Trigger::The `pull_request` event is not supported by Datadog Code Security and will cause issues with the product. ' +
           'To continue using Datadog Code Security, use the push event instead. See: https://docs.datadoghq.com/security/code_security/static_analysis/github_actions/#workflow\n for more information.'
       )
 
