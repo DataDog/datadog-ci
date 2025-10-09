2f1dd74d823430faa04cc6be859cf10ec7272a57
Cole Maring <cole.maring@datadoghq.com>
2025-10-09 14:24:11 -0400

    update tests

--- packages/plugin-sarif/src/__tests__/upload.test.ts
+++ packages/plugin-sarif/src/__tests__/upload.test.ts
@@ -189,16 +189,14 @@ describe('execute', () => {
   const runCLI = makeRunCLI(SarifUploadCommand, ['sarif', 'upload', '--env', 'ci', '--dry-run'])
 
   describe('CI event validation', () => {
-    test('should show warning for GitHub pull_request event', async () => {
+    test('should exit with error for GitHub pull_request event', async () => {
       const originalEnv = {...process.env}
       process.env.GITHUB_EVENT_NAME = 'pull_request'
-      process.env.DATADOG_API_KEY = 'fake-api-key'
 
       try {
         const context = createSimpleMockContext()
         const command = createCommand(SarifUploadCommand, context)
         command['basePaths'] = ['./src/__tests__/fixtures/subfolder']
-        command['dryRun'] = true
 
         const code = await command.execute()
         const output = context.stdout.toString()
@@ -214,16 +212,14 @@ describe('execute', () => {
       }
     })
 
-    test('should show warning for GitLab merge_request_event', async () => {
+    test('should exit with error for GitLab merge_request_event', async () => {
       const originalEnv = {...process.env}
       process.env.CI_PIPELINE_SOURCE = 'merge_request_event'
-      process.env.DATADOG_API_KEY = 'fake-api-key'
 
       try {
         const context = createSimpleMockContext()
         const command = createCommand(SarifUploadCommand, context)
         command['basePaths'] = ['./src/__tests__/fixtures/subfolder']
-        command['dryRun'] = true
 
         await command.execute()
         const output = context.stderr.toString()
@@ -237,16 +233,14 @@ describe('execute', () => {
       }
     })
 
-    test('should show warning for Azure PullRequest event', async () => {
+    test('should exit with error for Azure PullRequest event', async () => {
       const originalEnv = {...process.env}
       process.env.BUILD_REASON = 'PullRequest'
-      process.env.DATADOG_API_KEY = 'fake-api-key'
 
       try {
         const context = createSimpleMockContext()
         const command = createCommand(SarifUploadCommand, context)
         command['basePaths'] = ['./src/__tests__/fixtures/subfolder']
-        command['dryRun'] = true
 
         const code = await command.execute()
         const output = context.stdout.toString()
