271604a1d6fe1939245e408d3e27e18b18042fc3
Ava Silver <ava.silver@datadoghq.com>
2025-10-28 16:15:31 -0400

    [SVLS-7763] initial fixes

--- packages/base/src/commands/container-app/cli.ts
+++ packages/base/src/commands/container-app/cli.ts
@@ -1,9 +1,7 @@
 /* eslint-disable import-x/order */
 import {ContainerAppInstrumentCommand} from './instrument'
-import {ContainerAppUninstrumentCommand} from './uninstrument'
 
 // prettier-ignore
 export const commands = [
   ContainerAppInstrumentCommand,
-  ContainerAppUninstrumentCommand,
 ]
