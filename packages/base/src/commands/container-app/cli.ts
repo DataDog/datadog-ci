c00fa5226acf5671a71c1971f88388cc800a58d0
Ava Silver <ava.silver@datadoghq.com>
2025-10-30 13:45:24 -0400

    [SVLS-7763] package lint

--- packages/base/src/commands/container-app/cli.ts
+++ packages/base/src/commands/container-app/cli.ts
@@ -1,7 +1,9 @@
 /* eslint-disable import-x/order */
+import {ContainerAppCommonCommand} from './common'
 import {ContainerAppInstrumentCommand} from './instrument'
 
 // prettier-ignore
 export const commands = [
+  ContainerAppCommonCommand,
   ContainerAppInstrumentCommand,
 ]
