21a37027549daa9edc1472413c64bd007d93d2e1
Ava Silver <ava.silver@datadoghq.com>
2025-11-07 15:23:59 -0500

    [SVLS] fix cloud run label format

--- packages/plugin-cloud-run/src/commands/instrument.ts
+++ packages/plugin-cloud-run/src/commands/instrument.ts
@@ -263,7 +263,7 @@ export class PluginCommand extends CloudRunInstrumentCommand {
     const updatedLabels: Record<string, string> = {
       ...service.labels,
       service: ddService,
-      [SERVERLESS_CLI_VERSION_TAG_NAME]: SERVERLESS_CLI_VERSION_TAG_VALUE,
+      [SERVERLESS_CLI_VERSION_TAG_NAME]: SERVERLESS_CLI_VERSION_TAG_VALUE.replace('.', '_'),
     }
     if (!!this.environment) {
       updatedLabels.env = this.environment
