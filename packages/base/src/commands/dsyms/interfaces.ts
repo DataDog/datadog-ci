9a10e9a645540c3c90e48f5544776851823e5429
Maxime Epain <maxime.epain@datadoghq.com>
2025-12-02 18:35:14 +0100

    Add git metadata to dsyms cmd

--- packages/base/src/commands/dsyms/interfaces.ts
+++ packages/base/src/commands/dsyms/interfaces.ts
@@ -10,13 +10,20 @@ export interface DWARF {
   arch: string
 }
 
+export interface GitData {
+  repositoryURL: string
+  commitSHA: string
+}
+
 export class CompressedDsym {
   public archivePath: string
   public dsym: Dsym
+  public gitData?: GitData
 
-  constructor(archivePath: string, dsym: Dsym) {
+  constructor(archivePath: string, dsym: Dsym, gitData?: GitData) {
     this.archivePath = archivePath
     this.dsym = dsym
+    this.gitData = gitData
   }
 
   public asMultipartPayload(): MultipartPayload {
@@ -33,16 +40,23 @@ export class CompressedDsym {
   private getMetadataPayload(): MultipartValue {
     const concatUUIDs = this.dsym.dwarf.map((d) => d.uuid).join()
 
+    const metadata: {[k: string]: any} = {
+      type: 'ios_symbols',
+      uuids: concatUUIDs,
+    }
+
+    if (this.gitData) {
+      metadata.git_repository_url = this.gitData.repositoryURL
+      metadata.git_commit_sha = this.gitData.commitSHA
+    }
+
     return {
       type: 'string',
       options: {
         contentType: 'application/json',
         filename: 'event',
       },
-      value: JSON.stringify({
-        type: 'ios_symbols',
-        uuids: concatUUIDs,
-      }),
+      value: JSON.stringify(metadata),
     }
   }
 }
