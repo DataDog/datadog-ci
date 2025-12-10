db56c944b44cebef89e28b302b1fdc050e2368b1
corentin <corentin.girard@datadoghq.com>
2025-12-10 17:28:24 +0100

    [chores] Remove `git-metadata` exports entry

--- packages/datadog-ci/src/index.ts
+++ packages/datadog-ci/src/index.ts
@@ -1,3 +1,3 @@
-export * as gitMetadata from '@datadog/datadog-ci-base/commands/git-metadata'
+export * as gitMetadata from '@datadog/datadog-ci-base/commands/git-metadata/library'
 export * as utils from '@datadog/datadog-ci-base/helpers/utils'
 export {cliVersion as version} from '@datadog/datadog-ci-base/version'
