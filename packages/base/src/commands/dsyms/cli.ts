c4a5464ca46f6f7c12838e5f472a66a70a1f5196
corentin <corentin.girard@datadoghq.com>
2025-12-10 16:49:37 +0100

    Use lint script

--- packages/base/src/commands/dsyms/cli.ts
+++ packages/base/src/commands/dsyms/cli.ts
@@ -1,3 +1,7 @@
+/* eslint-disable import-x/order */
 import {DsymsUploadCommand} from './upload'
 
-export const commands = [DsymsUploadCommand]
+// prettier-ignore
+export const commands = [
+  DsymsUploadCommand,
+]
