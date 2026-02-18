3bfe135c08bb0c7b0140107832e0f95816fa21d3
Corentin Girard <corentin.girard@datadoghq.com>
2026-02-18 10:30:14 +0100

    [chores] Move `elf-symbols` and `unity-symbols` to base package (#2101)
    * Run `mv packages/datadog-ci/src/commands/elf-symbols packages/base/src/commands`
    
    * Adjust `lint-packages.ts`
    
    * Update `cli.ts` files
    
    * Run `yarn format`
    
    * Run `mv packages/datadog-ci/src/commands/unity-symbols packages/base/src/commands`
    
    * Update `cli.ts` files
    
    * Run `yarn format`

--- packages/base/src/cli.ts
+++ packages/base/src/cli.ts
@@ -1,6 +1,8 @@
 /* eslint-disable quote-props */
 import type {RecordWithKebabCaseKeys} from '@datadog/datadog-ci-base/helpers/types'
 
+// DO NOT EDIT MANUALLY. Update the source of truth in `bin/lint-packages.ts` instead.
+
 import {commands as aasCommands} from './commands/aas/cli'
 import {commands as cloudRunCommands} from './commands/cloud-run/cli'
 import {commands as containerAppCommands} from './commands/container-app/cli'
@@ -8,6 +10,7 @@ import {commands as coverageCommands} from './commands/coverage/cli'
 import {commands as deploymentCommands} from './commands/deployment/cli'
 import {commands as doraCommands} from './commands/dora/cli'
 import {commands as dsymsCommands} from './commands/dsyms/cli'
+import {commands as elfSymbolsCommands} from './commands/elf-symbols/cli'
 import {commands as gateCommands} from './commands/gate/cli'
 import {commands as gitMetadataCommands} from './commands/git-metadata/cli'
 import {commands as junitCommands} from './commands/junit/cli'
@@ -18,6 +21,9 @@ import {commands as sbomCommands} from './commands/sbom/cli'
 import {commands as stepfunctionsCommands} from './commands/stepfunctions/cli'
 import {commands as syntheticsCommands} from './commands/synthetics/cli'
 import {commands as tagCommands} from './commands/tag/cli'
+import {commands as unitySymbolsCommands} from './commands/unity-symbols/cli'
+
+// DO NOT EDIT MANUALLY. Update the source of truth in `bin/lint-packages.ts` instead.
 
 // prettier-ignore
 export const commands = {
@@ -28,6 +34,7 @@ export const commands = {
   'deployment': deploymentCommands,
   'dora': doraCommands,
   'dsyms': dsymsCommands,
+  'elf-symbols': elfSymbolsCommands,
   'gate': gateCommands,
   'git-metadata': gitMetadataCommands,
   'junit': junitCommands,
@@ -38,11 +45,19 @@ export const commands = {
   'stepfunctions': stepfunctionsCommands,
   'synthetics': syntheticsCommands,
   'tag': tagCommands,
+  'unity-symbols': unitySymbolsCommands,
 } satisfies RecordWithKebabCaseKeys
+
+// DO NOT EDIT MANUALLY. Update the source of truth in `bin/lint-packages.ts` instead.
 
 /**
  * Some command scopes do not have a plugin package, and their logic is entirely included in `@datadog/datadog-ci-base`.
  */
-export const noPluginExceptions: Set<string> = new Set(['dsyms', 'git-metadata', 'plugin', 'tag']) satisfies Set<
-  keyof typeof commands
->
+export const noPluginExceptions: Set<string> = new Set([
+  'dsyms',
+  'elf-symbols',
+  'git-metadata',
+  'plugin',
+  'tag',
+  'unity-symbols',
+]) satisfies Set<keyof typeof commands>
