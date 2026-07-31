7c3cda5f74150412daeceab1df8367b59bcb3bd0
Ava Silver <ava.silver@datadoghq.com>
2026-07-31 17:06:26 -0400

    [SVLS-9306] organize shared runtime copy

---
+++ packages/base/src/helpers/serverless/ssi/constants.ts
@@ -0,0 +1,4 @@
+export const TRACER_COPY_CONTAINER_NAME = 'datadog-tracer-copy'
+export const TRACER_VOLUME_NAME = 'datadog-tracer'
+export const TRACER_MOUNT_PATH = '/datadog-lib'
+export const TRACER_READINESS_PORT = 18999
