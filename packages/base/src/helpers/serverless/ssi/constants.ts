f171f1c989cbd01ec04ee725e0276fa28544c7f9
Ava Silver <ava.silver@datadoghq.com>
2026-07-31 17:28:32 -0400

    [SVLS-9302] validate cloud run ssi

--- packages/base/src/helpers/serverless/ssi/constants.ts
+++ packages/base/src/helpers/serverless/ssi/constants.ts
@@ -1,4 +1,15 @@
 export const TRACER_COPY_CONTAINER_NAME = 'datadog-tracer-copy'
+export const SSI_APP_CONTAINER_NAME = 'datadog-app'
 export const TRACER_VOLUME_NAME = 'datadog-tracer'
 export const TRACER_MOUNT_PATH = '/datadog-lib'
 export const TRACER_READINESS_PORT = 18999
+
+export const DEFAULT_TRACER_REGISTRY = 'gcr.io/datadoghq'
+export const DEFAULT_TRACER_VERSION = 'latest'
+export const DEFAULT_TRACER_LIBC = 'glibc'
+
+export const DEFAULT_TRACER_VOLUME_SIZE = '768Mi'
+export const DEFAULT_TRACER_SIDECAR_MEMORY = '1Gi'
+
+export const SSI_ADOPTION_LABEL_NAME = 'dd_sls_injection_mode'
+export const SSI_ADOPTION_LABEL_VALUE = 'single_language'
