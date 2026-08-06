81080e0904bbe527b249752cb12751a0b3dea307
Ava Silver <ava.silver@datadoghq.com>
2026-08-06 15:05:03 -0400

    [SVLS-9302] support all tracer images

--- packages/plugin-cloud-run/src/__tests__/instrument.test.ts
+++ packages/plugin-cloud-run/src/__tests__/instrument.test.ts
@@ -140,7 +140,6 @@ describe('InstrumentCommand', () => {
       [['--tracing', 'inject'], 'requires --language'],
       [['--tracer-version', 'latest'], 'require --tracing inject'],
       [['--tracing', 'inject', '--language', 'go'], 'dd-trace-go'],
-      [['--tracing', 'inject', '--language', 'nodejs'], 'startup probe'],
     ])('rejects incompatible options before network calls: %s', async (flags, expected) => {
       const {code, context} = await runCLI([...requiredFlags, ...flags])
 
