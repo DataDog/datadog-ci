87a5bfbb38f91fc2ad343e6cc46e2ffdcc0ab040
Hugo Silva <hugo.silva@datadoghq.com>
2026-08-27 10:39:13 +0200

    fix(sourcemaps): preserve command registry format

--- packages/base/src/commands/sourcemaps/cli.ts
+++ packages/base/src/commands/sourcemaps/cli.ts
@@ -1,5 +1,11 @@
+/* eslint-disable import-x/order */
 import {SourcemapsInjectCommand} from './inject'
 import {SourcemapsResolveCommand} from './resolve'
 import {SourcemapsUploadCommand} from './upload'
 
-export const commands = [SourcemapsInjectCommand, SourcemapsResolveCommand, SourcemapsUploadCommand]
+// prettier-ignore
+export const commands = [
+  SourcemapsInjectCommand,
+  SourcemapsResolveCommand,
+  SourcemapsUploadCommand,
+]
