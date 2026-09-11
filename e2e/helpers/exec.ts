3d2f301bf5a20aa1df1ba9d34e3c9abfad2ae364
Ava Silver <ava.silver@datadoghq.com>
2026-09-11 11:10:17 -0400

    [SVLS-9317] retry Azure provisioning delays

--- e2e/helpers/exec.ts
+++ e2e/helpers/exec.ts
@@ -47,6 +47,7 @@ const RETRYABLE_PATTERNS = [
   'RESOURCE_EXHAUSTED',
   'UNAVAILABLE',
   'temporarily unavailable',
+  'status code 404',
   // The Kudu/SCM site returns a 502 gateway error on the first deploy while it is still cold
   'acting as a gateway or proxy',
 ]
